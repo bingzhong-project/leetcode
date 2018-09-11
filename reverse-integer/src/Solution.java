a79d67bc9463f2cd7372dd3da4da17ce4f438b11
zhongyongbin <zhongyongbin@foxmail.com>
2018-09-11 23:06:32 +0800

    反转数

---
+++ reverse-integer/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        long res = 0;
+        while (x != 0) {
+            long num = x % 10;
+            res = res * 10 + num;
+            x /= 10;
+            if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+
+        return (int) res;
+    }
+}
