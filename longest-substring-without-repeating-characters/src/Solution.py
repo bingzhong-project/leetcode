97717500568a0274d44c6b32638aa30a709917ed
zhongyongbin <zhongyongbin@foxmail.com>
2018-09-12 21:04:53 +0800

    优化最长不重复子串

--- longest-substring-without-repeating-characters/src/Solution.py
+++ longest-substring-without-repeating-characters/src/Solution.py
@@ -4,23 +4,12 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        cache = dict()
         res = 0
-        str_len = len(s)
-        cursor = 0
-        while cursor < str_len:
-            now_cursor = cursor
-            now_str = s[cursor]
+        start = 0
+        cache = dict()
+        for i, now_str in enumerate(s):
             if now_str in cache:
-                new_res = len(cache)
-                if res < new_res:
-                    res = new_res
-                cursor = cache[now_str]
-                cache = dict()
-            else:
-                cache[now_str] = now_cursor
-            cursor = cursor + 1
-        new_res = len(cache)
-        if res < new_res:
-            res = new_res
+                start = cache[now_str] + 1
+            cache[now_str] = i
+            res = max(res, i - start + 1)
         return res
