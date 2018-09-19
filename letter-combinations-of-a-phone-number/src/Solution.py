08191c92ee05c8a33e948adcedf5296bfdbadb99
zhongyongbin <zhongyongbin@foxmail.com>
2018-09-19 13:59:09 +0800

    letterCombinations 初步完成

---
+++ letter-combinations-of-a-phone-number/src/Solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        phone_dict = {
+            '1': '',
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        if len(digits) == 0:
+            return []
+        result = list(phone_dict[digits[0]])
+        for i in range(1, len(digits)):
+            cache = list()
+            letters = phone_dict[digits[i]]
+            if len(letters) > 0:
+                for letter in letters:
+                    for i in range(len(result)):
+                        cache.append(result[i] + letter)
+                result = cache
+        return result
