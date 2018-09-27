83557174fc357b9712c0e36fc84a2500706727bb
zhongyongbin <zhongyongbin@foxmail.com>
2018-09-27 21:37:24 +0800

    完成

--- combination-sum/src/Solution.py
+++ combination-sum/src/Solution.py
@@ -8,19 +8,18 @@ class Solution:
 
         def dfs(candidates, target, candidate_sum, result, results):
             if candidate_sum == target:
-                results.append(result)
+                result = sorted(result)
+                if result not in results:
+                    results.append(result)
                 return
             for i in range(len(candidates)):
                 candidate = candidates[i]
                 if candidate_sum + candidate <= target:
                     result.append(candidate)
-                    candidate_sum += candidate
-                    dfs(candidates, target, candidate_sum, result[:], results)
+                    dfs(candidates, target, candidate_sum + candidate,
+                        result[:], results)
+                    result.pop()
 
         results = []
         dfs(candidates, target, 0, [], results)
         return results
-
-
-if __name__ == '__main__':
-    print(Solution().combinationSum([2, 3, 6, 7], 7))
