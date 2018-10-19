7abea38cce00d6dbd77167b990d0005e82795fbb
zhongyongbin <zhongyongbin@foxmail.com>
2018-10-19 23:53:18 +0800

    完成

--- jump-game/src/Solution.py
+++ jump-game/src/Solution.py
@@ -4,3 +4,9 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
+        reach = 0
+        for i in range(len(nums)):
+            if i > reach or reach >= len(nums) - 1:
+                break
+            reach = max(reach, i + nums[i])
+        return reach >= len(nums) - 1
