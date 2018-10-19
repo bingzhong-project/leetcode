85bad552d168a988280a1579782f05ebfd47561b
zhongyongbin <zhongyongbin@foxmail.com>
2018-10-19 19:44:44 +0800

    初始化

--- jump-game/src/Solution.py
+++ jump-game/src/Solution.py
@@ -4,21 +4,3 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
-        result = False
-        cur_position = 0
-        while cur_position < len(nums):
-            num = nums[cur_position]
-            if num == 0 and cur_position != len(nums) - 1:
-                break
-            if num + cur_position >= len(nums) - 1:
-                result = True
-                break
-            position_num = -1
-            add_position_num = 0
-            for i in range(1, num + 1):
-                if position_num <= nums[cur_position + i]:
-                    add_position_num = i
-                    position_num = nums[cur_position + i]
-            cur_position += add_position_num
-
-        return result
