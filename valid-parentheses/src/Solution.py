3319c3f66e8011010b01247882a59c639decaa1f
zhongyongbin <zhongyongbin@foxmail.com>
2018-10-23 18:24:13 +0800

    完成

---
+++ valid-parentheses/src/Solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        parentheses = {"(": 1, "[": 2, "{": 3, ")": -1, "]": -2, "}": -3}
+        stack = []
+        for p in s:
+            if len(stack) == 0 and parentheses[p] < 0:
+                return False
+            if len(stack) > 0:
+                top = stack[-1]
+                if parentheses[top] + parentheses[p] == 0:
+                    stack.pop()
+                    continue
+                if parentheses[top] * parentheses[p] < 0:
+                    return False
+            stack.append(p)
+        return len(stack) == 0
+
+
+if __name__ == '__main__':
+    print(Solution().isValid("(]]"))
