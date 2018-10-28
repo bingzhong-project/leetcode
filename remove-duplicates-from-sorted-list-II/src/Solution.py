bc75069bcd852eb5d346908f8242b6260b7a4957
zhongyongbin <zhongyongbin@foxmail.com>
2018-10-28 14:45:07 +0800

    完成

--- remove-duplicates-from-sorted-list-II/src/Solution.py
+++ remove-duplicates-from-sorted-list-II/src/Solution.py
@@ -11,3 +11,23 @@ class Solution:
         :type head: ListNode
         :rtype: ListNode
         """
+        dummy = ListNode(-1)
+        dummy.next = head
+        start = dummy
+        end = None
+        while start is not None:
+            node = start.next
+            times = 0
+            while node is not None:
+                end = node.next
+                if end is None:
+                    break
+                if node.val != end.val:
+                    break
+                node = node.next
+                times += 1
+            if times == 0:
+                start = start.next
+            else:
+                start.next = end
+        return dummy.next
