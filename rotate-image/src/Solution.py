b82befb86bf22e613e9ff3cd5ada8355bf098380
zhongyongbin <zhongyongbin@foxmail.com>
2018-10-30 14:11:05 +0800

    完成

--- rotate-image/src/Solution.py
+++ rotate-image/src/Solution.py
@@ -4,3 +4,19 @@ class Solution:
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
         """
+        up = 0
+        down = len(matrix) - 1
+        while up < down:
+            up_row = matrix[up]
+            down_row = matrix[down]
+            matrix[up] = down_row
+            matrix[down] = up_row
+            up += 1
+            down -= 1
+        for i in range(len(matrix)):
+            row = matrix[i]
+            for j in range(len(row)):
+                if i != j and i < j:
+                    tmp = matrix[i][j]
+                    matrix[i][j] = matrix[j][i]
+                    matrix[j][i] = tmp
