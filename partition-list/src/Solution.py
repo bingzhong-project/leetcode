24a0191b1c21e963351a5407933dab8b061c0028
zhongyongbin <zhongyongbin@foxmail.com>
2018-11-04 18:59:40 +0800

    完成

--- partition-list/src/Solution.py
+++ partition-list/src/Solution.py
@@ -12,3 +12,26 @@ class Solution:
         :type x: int
         :rtype: ListNode
         """
+        dummy = ListNode(-1)
+        dummy.next = head
+        front = dummy
+        back = front.next
+        while back is not None:
+            if back.val < x:
+                front = front.next
+                back = back.next
+            else:
+                break
+        insert = front
+        while back is not None:
+            if back.val >= x:
+                front = front.next
+                back = back.next
+            else:
+                front.next = back.next
+                insert_next = insert.next
+                insert.next = back
+                back.next = insert_next
+                back = front.next
+                insert = insert.next
+        return dummy.next
