733b597e2ef8bc9723ab502b7bb030eca898c5e0
zhongyongbin <zhongyongbin@foxmail.com>
2018-11-06 19:31:20 +0800

    init 思路

---
+++ restore-ip-addresses/solutions.md
@@ -0,0 +1,6 @@
+> Restore IP Addresses 解题思路
+
+# 知识点
+回溯算法
+
+# 解题思路
--- restore-ip-addresses/src/Solution.py
+++ restore-ip-addresses/src/Solution.py
@@ -11,6 +11,9 @@ class Solution:
             num = int(s)
             return num > 0 and num <= 255
 
+        def is_continue(s, count):
+            return 4 - count <= len(s) <= 12 - count
+
         def dfs(s, ip, count, res):
             if count == 3 and is_valid(s):
                 res.append(ip + s)
@@ -18,7 +21,7 @@ class Solution:
             i = 1
             while i < 4 and i < len(s):
                 temp = s[:i]
-                if is_valid(temp):
+                if is_valid(temp) and is_continue(s[i:], count + 1):
                     dfs(s[i:], ip + temp + '.', count + 1, res)
                 i += 1
 
