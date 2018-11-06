5d0d047e86619a730442cdaf890cc0d1b0a1edbb
zhongyongbin <zhongyongbin@foxmail.com>
2018-11-06 13:57:22 +0800

    完成

--- restore-ip-addresses/src/Solution.py
+++ restore-ip-addresses/src/Solution.py
@@ -4,3 +4,26 @@ class Solution:
         :type s: str
         :rtype: List[str]
         """
+
+        def is_valid(s):
+            if s[0] == '0':
+                return s == '0'
+            num = int(s)
+            return num > 0 and num <= 255
+
+        def dfs(s, ip, count, res):
+            if count == 3 and is_valid(s):
+                res.append(ip + s)
+                return
+            i = 1
+            while i < 4 and i < len(s):
+                temp = s[:i]
+                if is_valid(temp):
+                    dfs(s[i:], ip + temp + '.', count + 1, res)
+                i += 1
+
+        if len(s) < 4 or len(s) > 12:
+            return []
+        res = []
+        dfs(s, '', 0, res)
+        return res
