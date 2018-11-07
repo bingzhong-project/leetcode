940a630f0d1f8ee0c28073c87b27ea8c1c492b3a
zhongyongbin <zhongyongbin@foxmail.com>
2018-11-07 17:57:41 +0800

    完成

---
+++ binary-tree-postorder-traversal/src/Solution.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if root is None:
+            return res
+        stack = []
+        visited = []
+        stack.append(root)
+        while len(stack) > 0:
+            node = stack[-1]
+            if (node.left is None
+                    or node.left in visited) and (node.right is None
+                                                  or node.right in visited):
+                res.append(stack.pop().val)
+            if node.left is not None and node.left not in visited:
+                stack.append(node.left)
+                visited.append(node.left)
+            elif node.right is not None and node.right not in visited:
+                stack.append(node.right)
+                visited.append(node.right)
+        return res
