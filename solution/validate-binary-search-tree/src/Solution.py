17552a7c8bd6ff30a7a7efceac90c860f6372007
zhongyongbin <zhongyongbin@foxmail.com>
2018-11-08 22:47:11 +0800

    完成

--- solution/validate-binary-search-tree/src/Solution.py
+++ solution/validate-binary-search-tree/src/Solution.py
@@ -12,3 +12,15 @@ class Solution:
         :type root: TreeNode
         :rtype: bool
         """
+
+        def dfs(node, min_val, max_val):
+            if node is None:
+                return True
+            if (min_val is not None
+                    and node.val <= min_val) or (max_val is not None
+                                                 and node.val >= max_val):
+                return False
+            return dfs(node.left, min_val, node.val) and dfs(
+                node.right, node.val, max_val)
+
+        return dfs(root, None, None)
