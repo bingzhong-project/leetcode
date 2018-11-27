85829d1d79ba8b3508faa893cc34d6901fc3ed6b
zhongyongbin <zhongyongbin@foxmail.com>
2018-11-27 11:44:50 +0800

    优化

--- solution/largest-divisible-subset/src/Solution2.py
+++ solution/largest-divisible-subset/src/Solution2.py
@@ -10,15 +10,15 @@ class Solution:
         max_index = 0
         subset_index = [0 for _ in range(len(nums))]
         dp = [0 for _ in range(len(nums))]
-        for i in range(len(nums) - 1, -1, -1):
-            for j in range(i, len(nums)):
-                if nums[j] % nums[i] == 0 and dp[i] < dp[j] + 1:
+        for i in range(len(nums)):
+            for j in range(i, -1, -1):
+                if nums[i] % nums[j] == 0 and dp[i] < dp[j] + 1:
                     dp[i] = dp[j] + 1
                     subset_index[i] = j
                     if dp[max_index] < dp[i]:
                         max_index = i
         res = []
-        for i in range(len(subset_index)):
+        for i in range(len(subset_index) - 1, -1, -1):
             if i == max_index:
                 res.append(nums[max_index])
                 max_index = subset_index[max_index]
