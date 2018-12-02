46f9fd286e9c473e7f1a346f4768f0ed5ca34afa
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-02 21:18:56 +0800

    完成

--- solution/combination-sum-iv/src/Solution.py
+++ solution/combination-sum-iv/src/Solution.py
@@ -5,3 +5,10 @@ class Solution:
         :type target: int
         :rtype: int
         """
+        dp = [0 for _ in range(target + 1)]
+        dp[0] = 1
+        for i in range(1, target + 1):
+            for num in nums:
+                if i >= num:
+                    dp[i] += dp[i - num]
+        return dp[-1]
