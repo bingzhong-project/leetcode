55c8eec2c75f923c554d02e42a222198f22095bf
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-13 00:42:05 +0800

    test

--- algorithms/sort-list/src/Solution2.py
+++ algorithms/sort-list/src/Solution2.py
@@ -25,28 +25,42 @@ class Solution:
             return prev
 
         def sort(head, tail):
-            if head.next is None:
+            if head is None or head.next is None:
                 return
             dummy = ListNode(None)
             dummy.next = head
-            prev_middle_node = middle(head)
-            middle_node = prev_middle_node.next
+            middle_node = middle(head)
 
             i = dummy.next
             while i != middle_node:
+                i_next = i.next
                 if i.val > middle_node.val:
                     middle_node_next = middle_node.next
                     i_next = i.next
-                    middle_node.next, i.next, i = i, middle_node_next, i_next
+                    middle_node.next, i.next = i, middle_node_next
+                i = i_next
 
             j = middle_node.next
-            while j.next != tail:
+            while j is not None and j.next != tail:
+                j_next = j.next
                 if j.val < middle_node.val:
                     dummy_next = dummy.next
                     j_next = j.next
-                    dummy.next, j.next, j = j, dummy_next, j_next
-            sort(head, middle_node)
+                    dummy.next, j.next = j, dummy_next
+                j = j_next
+            sort(dummy.next, middle_node)
             sort(middle_node.next, None)
 
-        sort(head)
+        sort(head, None)
         return head
+
+
+# if __name__ == '__main__':
+#     node1 = ListNode(1)
+#     node2 = ListNode(2)
+#     node3 = ListNode(3)
+#     node4 = ListNode(4)
+#     node4.next = node2
+#     node2.next = node1
+#     node1.next = node3
+#     Solution().sortList(node4)
