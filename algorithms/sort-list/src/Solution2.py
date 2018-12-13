6373490ad800c11416bef33dc9ef0f6fef4cc50d
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-13 20:24:10 +0800

    待完成

--- algorithms/sort-list/src/Solution2.py
+++ algorithms/sort-list/src/Solution2.py
@@ -14,22 +14,33 @@ class Solution:
         :rtype: ListNode
         """
 
-        def middle(node, tail):
-            slow = node
-            fast = node
-            while fast != tail and fast.next != tail:
-                slow = slow.next
-                fast = fast.next.next
-            return slow
-
-        def swap(node1, prev_node1, node2, prev_node2):
-            prev_node1.next, prev_node2.next = node2, node1
-            node1.next, node2.next = node2.next, node1.next
+        def swap(node1, node2):
+            tmp = node1.val
+            node1.val = node2.val
+            node2.val = tmp
 
         def sort(head, tail):
-            if head == tail:
+            if head == tail or head.next is None:
                 return
-            pass
+            val = head.val
+            switch = head
+            i = head.next
+            while i != tail:
+                if i.val < val:
+                    switch = switch.next
+                    if switch != i:
+                        swap(i, switch)
 
         sort(head, None)
         return head
+
+
+if __name__ == '__main__':
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+    node4.next = node2
+    node2.next = node1
+    node1.next = node3
+    Solution().sortList(node4)
