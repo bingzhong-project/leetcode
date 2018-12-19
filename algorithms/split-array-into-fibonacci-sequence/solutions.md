76ec6626e6d988b15cb3b2fa4303afb0f98a5206
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-19 16:16:54 +0800

    init

---
+++ algorithms/split-array-into-fibonacci-sequence/solutions.md
@@ -0,0 +1,3 @@
+# Split Array into Fibonacci Sequence
+
+[问题描述](https://leetcode.com/problems/split-array-into-fibonacci-sequence/)
---
+++ algorithms/split-array-into-fibonacci-sequence/src/Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def splitIntoFibonacci(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
