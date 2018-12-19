346d531eae881ebbc54ee556b5c1c4f466fb1b9b
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-19 17:16:06 +0800

    finish

--- algorithms/split-array-into-fibonacci-sequence/src/Solution.py
+++ algorithms/split-array-into-fibonacci-sequence/src/Solution.py
@@ -4,3 +4,25 @@ class Solution:
         :type S: str
         :rtype: List[int]
         """
+
+        max_value = "2147483648"
+
+        def dfs(string, paths, limit_factory, res):
+            if len(res) > 0:
+                return
+            if len(string) == 0 and len(paths) >= 3:
+                res.extend(paths)
+                return
+            for i in range(1, len(string) + 1):
+                num_str = string[:i]
+                if len(max_value) <= len(num_str) and num_str > max_value:
+                    break
+                if (len(num_str) > 1 and num_str.startswith("0")):
+                    break
+                num = int(num_str)
+                if len(paths) < 2 or num == paths[-1] + paths[-2]:
+                    dfs(string[i:], paths + [num], limit_factory - 1, res)
+
+        res = []
+        dfs(S, [], 3, res)
+        return res
