c745e2ec51f4415109f817989c419318f7b24662
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-21 16:20:03 +0800

    优化

--- algorithms/beautiful-arrangement/src/Solution.py
+++ algorithms/beautiful-arrangement/src/Solution.py
@@ -8,13 +8,11 @@ class Solution:
         def dfs(numbers, i, res):
             if len(numbers) == 0:
                 res[0] += 1
-            for number in numbers:
+            for pos, number in enumerate(numbers):
                 if number % i == 0 or i % number == 0:
-                    new_numbers = numbers.copy()
-                    new_numbers.remove(number)
-                    dfs(new_numbers, i + 1, res)
+                    dfs(numbers[:pos] + numbers[pos + 1:], i + 1, res)
 
-        numbers = {n for n in range(1, N + 1)}
+        numbers = [n for n in range(1, N + 1)]
         res = [0]
         dfs(numbers, 1, res)
         return res[0]
