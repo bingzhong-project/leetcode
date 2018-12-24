c3a764269971e0c6e3d9032a3d251ecd6eb24de1
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-24 17:54:15 +0800

    init

---
+++ algorithms/number-of-islands/solutions.md
@@ -0,0 +1,3 @@
+# Number of Islands
+
+[问题描述](https://leetcode.com/problems/number-of-islands/)
---
+++ algorithms/number-of-islands/src/Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
