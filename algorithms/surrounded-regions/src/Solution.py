1ffad1faca21e649d9f9512d91a998578f8e7a7c
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-26 13:43:46 +0800

    finish

--- algorithms/surrounded-regions/src/Solution.py
+++ algorithms/surrounded-regions/src/Solution.py
@@ -5,27 +5,26 @@ class Solution:
         :rtype: void Do not return anything, modify board in-place instead.
         """
 
-        def dfs(board, visited, positions, i, j):
+        def dfs(board, i, j):
             if i > len(board) - 1 or i < 0 or j > len(board[-1]) - 1 or j < 0:
-                return True
-            if board[i][j] == 'X':
-                return False
-            if visited[i][j]:
-                return False
-            visited[i][j] = True
-            positions.append([i, j])
-            return dfs(board, visited, positions, i - 1, j) or dfs(
-                board, visited, positions, i + 1, j) or dfs(
-                    board, visited, positions, i, j - 1) or dfs(
-                        board, visited, positions, i, j + 1)
+                return
+            if board[i][j] != 'O':
+                return
+            board[i][j] = '$'
+            dfs(board, i - 1, j)
+            dfs(board, i + 1, j)
+            dfs(board, i, j - 1)
+            dfs(board, i, j + 1)
 
-        visited = [[False for _ in range(len(board[-1]))]
-                   for _ in range(len(board))]
         for i in range(len(board)):
             for j in range(len(board[-1])):
-                if board[i][j] == 'O' and not visited[i][j]:
-                    positions = list()
-                    flag = dfs(board, visited, positions, i, j)
-                    if not flag:
-                        for pos in positions:
-                            board[pos[0]][pos[1]] = 'X'
+                if (i == 0 or i == len(board) - 1 or j == 0
+                        or j == len(board[i]) - 1) and board[i][j] == 'O':
+                    dfs(board, i, j)
+
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                if board[i][j] == '$':
+                    board[i][j] = 'O'
