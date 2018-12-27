768dc899e7e253be5710d5726aa89533c36c6d4d
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-27 10:36:09 +0800

    init

---
+++ algorithms/course-schedule/solutions.md
@@ -0,0 +1,3 @@
+# Course Schedule
+
+[问题描述](https://leetcode.com/problems/course-schedule/)
---
+++ algorithms/course-schedule/src/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
