04628ec76af8a18abd7717cfb60784e35858bbfa
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-28 15:46:17 +0800

    finish

--- algorithms/course-schedule/src/Solution.py
+++ algorithms/course-schedule/src/Solution.py
@@ -5,3 +5,28 @@ class Solution:
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
+
+        def dfs(graph, visited, i):
+            if visited[i] == -1:
+                return False
+            if visited[i] == 1:
+                return True
+            visited[i] = -1
+            if i in graph:
+                for node in graph[i]:
+                    if not dfs(graph, visited, node):
+                        return False
+            visited[i] = 1
+            return True
+
+        graph = {}
+        visited = [0 for _ in range(numCourses)]
+        for prerequisite in prerequisites:
+            if prerequisite[1] not in graph:
+                graph[prerequisite[1]] = []
+            graph[prerequisite[1]].append(prerequisite[0])
+
+        for i in range(numCourses):
+            if not dfs(graph, visited, i):
+                return False
+        return True
