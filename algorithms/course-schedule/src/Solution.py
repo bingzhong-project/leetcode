bb5d6dce088ba7655f64fb845464c2ae4d5cc3c4
zhongyongbin <zhongyongbin@foxmail.com>
2018-12-28 18:03:07 +0800

    update

--- algorithms/course-schedule/src/Solution.py
+++ algorithms/course-schedule/src/Solution.py
@@ -12,18 +12,15 @@ class Solution:
             if visited[i] == 1:
                 return True
             visited[i] = -1
-            if i in graph:
-                for node in graph[i]:
-                    if not dfs(graph, visited, node):
-                        return False
+            for node in graph[i]:
+                if not dfs(graph, visited, node):
+                    return False
             visited[i] = 1
             return True
 
-        graph = {}
+        graph = [[] for i in range(numCourses)]
         visited = [0 for _ in range(numCourses)]
         for prerequisite in prerequisites:
-            if prerequisite[1] not in graph:
-                graph[prerequisite[1]] = []
             graph[prerequisite[1]].append(prerequisite[0])
 
         for i in range(numCourses):
