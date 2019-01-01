b583c9757eb33d527c2f0e81655c582d363fc043
zhongyongbin <zhongyongbin@foxmail.com>
2019-01-01 21:13:18 +0800

    finish

--- algorithms/decode-string/src/Solution.py
+++ algorithms/decode-string/src/Solution.py
@@ -4,3 +4,22 @@ class Solution:
         :type s: str
         :rtype: str
         """
+        nums = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+        left_bracket = "["
+        right_bracket = "]"
+
+        stack = []
+        for string in s:
+            if string == right_bracket:
+                tmp_str = ""
+                while stack[-1] != left_bracket:
+                    tmp_str = stack.pop() + tmp_str
+                stack.pop()
+                tmp_num = ""
+                while len(stack) > 0 and stack[-1] in nums:
+                    tmp_num = stack.pop() + tmp_num
+                tmp_res = int(tmp_num) * tmp_str
+                stack.append(tmp_res)
+            else:
+                stack.append(string)
+        return "".join(stack)
