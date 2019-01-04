ae6cbf809e77010b2e6ab07ff803cc87f9230c64
zhongyongbin <zhongyongbin@foxmail.com>
2019-01-04 15:14:48 +0800

    finish

--- algorithms/matchsticks-to-square/src/Solution.py
+++ algorithms/matchsticks-to-square/src/Solution.py
@@ -4,3 +4,24 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
+
+        def dfs(nums, target, index=0):
+            if index == len(nums):
+                return True
+
+            for i in range(4):
+                if target[i] - nums[index] < 0:
+                    continue
+                target[i] -= nums[index]
+                if (dfs(nums, target, index + 1)):
+                    return True
+                target[i] += nums[index]
+            return False
+
+        if len(nums) < 4:
+            return False
+        nums_sum = sum(nums)
+        if nums_sum % 4 != 0:
+            return False
+        nums.sort(reverse=True)
+        return dfs(nums, [nums_sum // 4] * 4)
