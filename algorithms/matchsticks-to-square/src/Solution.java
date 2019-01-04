5d237cd92474eb5e1c6e8144bd51d4a9b140db37
zhongyongbin <zhongyongbin@foxmail.com>
2019-01-04 23:12:31 +0800

    finish java

--- algorithms/matchsticks-to-square/src/Solution.java
+++ algorithms/matchsticks-to-square/src/Solution.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean makesquare(int[] nums) {
-        return false;
+        if (nums.length < 4) {
+            return false;
+        }
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 4 != 0) {
+            return false;
+        }
+        int target = (int) (sum / 4);
+        int[] sides = { target, target, target, target };
+        Arrays.sort(nums);
+        return dfs(nums, sides, 0);
     }
 
     public boolean dfs(int[] nums, int[] sides, int index) {
@@ -9,7 +24,14 @@ class Solution {
         }
 
         for (int i = 0; i < 4; i++) {
-
+            if (sides[i] - nums[index] < 0) {
+                return false;
+            }
+            sides[i] -= nums[index];
+            if (dfs(nums, sides, index + 1)) {
+                return true;
+            }
+            sides[i] += nums[index];
         }
 
         return false;
