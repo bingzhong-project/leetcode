bb99df4441ea0b760fced084dcedcaa8773e381f
zhongyongbin <zhongyongbin@foxmail.com>
2019-01-04 19:23:00 +0800

    update

--- algorithms/matchsticks-to-square/src/Solution.java
+++ algorithms/matchsticks-to-square/src/Solution.java
@@ -2,4 +2,16 @@ class Solution {
     public boolean makesquare(int[] nums) {
         return false;
     }
+
+    public boolean dfs(int[] nums, int[] sides, int index) {
+        if (index == nums.length) {
+            return true;
+        }
+
+        for (int i = 0; i < 4; i++) {
+
+        }
+
+        return false;
+    }
 }
