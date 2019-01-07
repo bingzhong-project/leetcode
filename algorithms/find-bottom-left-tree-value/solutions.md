49d1f22cad63bc89d36e7269116c1e0060d681a9
zhongyongbin <zhongyongbin@foxmail.com>
2019-01-07 19:22:46 +0800

    finish

--- algorithms/find-bottom-left-tree-value/solutions.md
+++ algorithms/find-bottom-left-tree-value/solutions.md
@@ -1,3 +1,11 @@
 # Find Bottom Left Tree Value
 
 [问题描述](https://leetcode.com/problems/find-bottom-left-tree-value/)
+
+## 知识点
+
+深度优先搜索
+
+## 解题思路
+
+通过深度优先搜索遍历树，在遍历的同时记录遍历层数。当遇到叶子节点时，尝试记录叶子节点的值并记录当前层数值为 max_level，只有当前层数大于 max_level 才更新值。
--- algorithms/find-bottom-left-tree-value/src/Solution.py
+++ algorithms/find-bottom-left-tree-value/src/Solution.py
@@ -12,3 +12,20 @@ class Solution:
         :type root: TreeNode
         :rtype: int
         """
+
+        def dfs(node, level, max_level, res):
+            if node.left is None and node.right is None:
+                if level > max_level[0]:
+                    res[0] = node.val
+                    max_level[0] = level
+            if node.left is not None:
+                dfs(node.left, level + 1, max_level, res)
+            if node.right is not None:
+                dfs(node.right, level + 1, max_level, res)
+
+        if root is None:
+            return 0
+        res = [0]
+        dfs(root, 0, [-1], res)
+
+        return res[0]
