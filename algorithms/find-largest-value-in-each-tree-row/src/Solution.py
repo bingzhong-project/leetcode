26172094b35b8fac2e89108dc7a1977f06665471
zhongyongbin <zhongyongbin@foxmail.com>
2019-01-08 14:05:07 +0800

    finish

--- algorithms/find-largest-value-in-each-tree-row/src/Solution.py
+++ algorithms/find-largest-value-in-each-tree-row/src/Solution.py
@@ -12,3 +12,20 @@ class Solution:
         :type root: TreeNode
         :rtype: List[int]
         """
+        MIN_VALUE = -2**31
+
+        def dfs(node, level, res):
+            if level not in res:
+                res[level] = MIN_VALUE
+            res[level] = max(res[level], node.val)
+            if node.left is not None:
+                dfs(node.left, level + 1, res)
+            if node.right is not None:
+                dfs(node.right, level + 1, res)
+
+        if root is None:
+            return []
+        res = dict()
+        dfs(root, 0, res)
+
+        return list(res.values())
