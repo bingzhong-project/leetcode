db5f83a35bf44b2706ab3ef73b71976e0d9c2f1a
zhongyongbin <zhongyongbin@foxmail.com>
2019-01-16 16:03:30 +0800

    完成思路

--- algorithms/friend-circles/src/Solution.py
+++ algorithms/friend-circles/src/Solution.py
@@ -4,3 +4,18 @@ class Solution:
         :type M: List[List[int]]
         :rtype: int
         """
+
+        def dfs(graph, i):
+            if graph[i][i] != 1:
+                return
+            graph[i][i] = -1
+            for j in range(len(graph[i])):
+                if graph[i][j] == 1:
+                    dfs(graph, j)
+
+        res = 0
+        for i in range(len(M)):
+            if M[i][i] == 1:
+                res += 1
+                dfs(M, i)
+        return res
