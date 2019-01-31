36e67eb0c0c0fa43ce67e40260baf7de6bff7bfa
zhongyongbin <zhongyongbin@foxmail.com>
2019-01-31 17:44:19 +0800

    添加解法

--- algorithms/cheapest-flights-within-k-stops/src/Solution.py
+++ algorithms/cheapest-flights-within-k-stops/src/Solution.py
@@ -8,3 +8,26 @@ class Solution:
         :type K: int
         :rtype: int
         """
+        adj = [[] for _ in range(n)]
+        for flight in flights:
+            adj[flight[0]].append((flight[1], flight[2]))
+
+        res = 2**31
+        queue = []
+        queue.append((src, 0))
+        layer = -1
+
+        while len(queue) > 0:
+            for _ in range(len(queue)):
+                city, price = queue.pop(0)
+                if city == dst:
+                    res = min(res, price)
+                for next_city, next_price in adj[city]:
+                    if next_price + price > res:
+                        continue
+                    queue.append((next_city, next_price + price))
+            layer += 1
+            if layer > K:
+                break
+
+        return res if res != 2**31 else -1
