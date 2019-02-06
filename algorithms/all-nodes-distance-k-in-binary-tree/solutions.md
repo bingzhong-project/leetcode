8fb7912d1dd65b36df593d2d0fcff1d1f44e76f5
zhongyongbin <zhongyongbin@foxmail.com>
2019-02-06 12:14:27 +0800

    完成

--- algorithms/all-nodes-distance-k-in-binary-tree/solutions.md
+++ algorithms/all-nodes-distance-k-in-binary-tree/solutions.md
@@ -1,3 +1,11 @@
 # All Nodes Distance K in Binary Tree
 
 [问题描述](https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/)
+
+## 知识点
+
+深度优先搜索
+
+## 解题思路
+
+题目其实就是求单源路径中，符合指定长度的结点。但是题目给出的是二叉树，所以先要为每个结点标注它们的父结点，只要每个结点都可以指向其父结点，那么树就可以看成是一个“普通”的无向图，然后利用广度优先搜索找出符合路径长度的结点即可。
--- algorithms/all-nodes-distance-k-in-binary-tree/src/Solution.py
+++ algorithms/all-nodes-distance-k-in-binary-tree/src/Solution.py
@@ -14,3 +14,32 @@ class Solution:
         :type K: int
         :rtype: List[int]
         """
+
+        def traverse(node):
+            node.visited = False
+            if node.left is not None:
+                node.left.parent = node
+                traverse(node.left)
+            if node.right is not None:
+                node.right.parent = node
+                traverse(node.right)
+
+        root.parent = None
+        traverse(root)
+
+        queue = list()
+        queue.append(target)
+        layer = 0
+
+        while queue:
+            if layer == K:
+                return [e.val for e in queue]
+            layer += 1
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                node.visited = True
+                for vertex in (node.left, node.right, node.parent):
+                    if vertex is not None and not vertex.visited:
+                        queue.append(vertex)
+
+        return []
