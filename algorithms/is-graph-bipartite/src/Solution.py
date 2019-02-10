9885a73008fb5cbc332c203d2426d7a4bc92bb88
zhongyongbin <zhongyongbin@foxmail.com>
2019-02-10 19:07:17 +0800

    完成

--- algorithms/is-graph-bipartite/src/Solution.py
+++ algorithms/is-graph-bipartite/src/Solution.py
@@ -1,3 +1,20 @@
 class Solution:
     def isBipartite(self, graph: 'List[List[int]]') -> 'bool':
-        pass
+        color = [0 for _ in range(len(graph))]
+
+        for root in range(len(graph)):
+            if color[root] != 0:
+                continue
+            queue = []
+            queue.append(root)
+            color[root] = 1
+            while queue:
+                v = queue.pop(0)
+                for u in graph[v]:
+                    if color[u] == 0:
+                        color[u] = -color[v]
+                        queue.append(u)
+                    else:
+                        if color[v] == color[u]:
+                            return False
+        return True
