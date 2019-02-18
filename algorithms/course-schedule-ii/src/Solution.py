9cd215f2735c5f4a129bd17141cee425d7a1e653
zhongyongbin <zhongyongbin@foxmail.com>
2019-02-19 00:05:30 +0800

    update

--- algorithms/course-schedule-ii/src/Solution.py
+++ algorithms/course-schedule-ii/src/Solution.py
@@ -6,26 +6,26 @@ class Solution:
         :rtype: List[int]
         """
 
-        def dfs(graph, visited, topology_list, i):
+        def dfs(adj, visited, topology_list, i):
             if visited[i] == -1:
                 return False
             if visited[i] == 1:
                 return True
             visited[i] = -1
-            for node in graph[i]:
-                if not dfs(graph, visited, topology_list, node):
+            for node in adj[i]:
+                if not dfs(adj, visited, topology_list, node):
                     return False
             visited[i] = 1
             topology_list.append(i)
             return True
 
-        graph = [[] for i in range(numCourses)]
+        adj = [[] for i in range(numCourses)]
         visited = [0 for _ in range(numCourses)]
         for prerequisite in prerequisites:
-            graph[prerequisite[1]].append(prerequisite[0])
+            adj[prerequisite[1]].append(prerequisite[0])
 
         topology_list = []
         for i in range(numCourses):
-            if not dfs(graph, visited, topology_list, i):
+            if not dfs(adj, visited, topology_list, i):
                 return []
         return topology_list[::-1]
--- algorithms/course-schedule/src/Solution.py
+++ algorithms/course-schedule/src/Solution.py
@@ -6,24 +6,24 @@ class Solution:
         :rtype: bool
         """
 
-        def dfs(graph, visited, i):
+        def dfs(adj, visited, i):
             if visited[i] == -1:
                 return False
             if visited[i] == 1:
                 return True
             visited[i] = -1
-            for node in graph[i]:
-                if not dfs(graph, visited, node):
+            for node in adj[i]:
+                if not dfs(adj, visited, node):
                     return False
             visited[i] = 1
             return True
 
-        graph = [[] for i in range(numCourses)]
+        adj = [[] for i in range(numCourses)]
         visited = [0 for _ in range(numCourses)]
         for prerequisite in prerequisites:
-            graph[prerequisite[1]].append(prerequisite[0])
+            adj[prerequisite[1]].append(prerequisite[0])
 
         for i in range(numCourses):
-            if not dfs(graph, visited, i):
+            if not dfs(adj, visited, i):
                 return False
         return True
