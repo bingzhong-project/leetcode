5a32fb6013b2a38ae5586c2051c2cb7a58be321f
zhongyongbin <zhongyongbin@foxmail.com>
2019-02-23 10:59:04 +0800

    update

--- algorithms/next-greater-element-ii/src/Solution.py
+++ algorithms/next-greater-element-ii/src/Solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def nextGreaterElements(self, nums: 'List[int]') -> 'List[int]':
-        pass
+        res = [-1 for _ in range(len(nums))]
+        stack = []
+        for i in range(2 * len(nums)):
+            cursor = i % len(nums)
+            while stack and nums[cursor] > nums[stack[-1]]:
+                res[stack.pop()] = nums[cursor]
+            stack.append(cursor)
+        return res
