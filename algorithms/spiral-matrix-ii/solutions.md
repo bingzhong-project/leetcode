7b31f6045ea28965bc6e46b3ab7d6c34a061d5c9
zhongyongbin <zhongyongbin@foxmail.com>
2019-02-27 13:44:06 +0800

    完成

---
+++ algorithms/spiral-matrix-ii/solutions.md
@@ -0,0 +1,3 @@
+# Spiral Matrix II
+
+[问题描述](https://leetcode.com/problems/spiral-matrix-ii/)
---
+++ algorithms/spiral-matrix-ii/src/Solution.py
@@ -0,0 +1,39 @@
+class Solution:
+    def generateMatrix(self, n: 'int') -> 'List[List[int]]':
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        up = 0
+        down = n - 1
+        left = 0
+        right = n - 1
+        num = 1
+        while True:
+            for i in range(left, right + 1):
+                matrix[up][i] = num
+                num += 1
+            up += 1
+            if up > down:
+                break
+            for i in range(up, down + 1):
+                matrix[i][right] = num
+                num += 1
+            right -= 1
+            if left > right:
+                break
+            for i in range(right, left - 1, -1):
+                matrix[down][i] = num
+                num += 1
+            down -= 1
+            if up > down:
+                break
+            for i in range(down, up - 1, -1):
+                matrix[i][left] = num
+                num += 1
+            left += 1
+            if left > right:
+                break
+
+        return matrix
+
+
+if __name__ == '__main__':
+    print(Solution().generateMatrix(3))
