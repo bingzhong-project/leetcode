34d8ed5795e65c7cf9561bec4a5d441cd624b9fb
zhongyongbin <zhongyongbin@foxmail.com>
2019-02-28 11:15:07 +0800

    完成

--- algorithms/minimum-size-subarray-sum/src/Solution.py
+++ algorithms/minimum-size-subarray-sum/src/Solution.py
@@ -1,3 +1,13 @@
 class Solution:
     def minSubArrayLen(self, s: 'int', nums: 'List[int]') -> 'int':
-        pass
+        res = 2**31
+        left = 0
+        sum = 0
+        for i in range(len(nums)):
+            sum += nums[i]
+            while sum >= s:
+                res = min(res, i - left + 1)
+                sum -= nums[left]
+                left += 1
+
+        return 0 if res == 2**31 else res
