464afeac56a81e0b57806cc46e49eb86c9dc01e5
zhongyongbin <zhongyongbin@foxmail.com>
2019-03-01 19:11:44 +0800

    update

--- algorithms/decode-ways/src/Solution.py
+++ algorithms/decode-ways/src/Solution.py
@@ -1,3 +1,15 @@
 class Solution:
     def numDecodings(self, s: 'str') -> 'int':
-        pass
+        if len(s) == 0:
+            return 1
+
+        dp = [0 for _ in range(len(s) + 1)]
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, len(s) + 1):
+            num = int(s[i - 2:i])
+            if num <= 26:
+                dp[i] = dp[i - 1] + dp[i - 2]
+            else:
+                dp[i] = dp[i - 1]
+        return dp[-1]
