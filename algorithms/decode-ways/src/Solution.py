c9e2d411bb38a05c6e74bba32cc271d423020567
zhongyongbin <zhongyongbin@foxmail.com>
2019-03-02 19:54:58 +0800

    完成

--- algorithms/decode-ways/src/Solution.py
+++ algorithms/decode-ways/src/Solution.py
@@ -2,14 +2,22 @@ class Solution:
     def numDecodings(self, s: 'str') -> 'int':
         if len(s) == 0:
             return 1
+        if s[0] == '0':
+            return 0
 
         dp = [0 for _ in range(len(s) + 1)]
         dp[0] = 1
         dp[1] = 1
-        for i in range(2, len(s) + 1):
-            num = int(s[i - 2:i])
-            if num <= 26:
-                dp[i] = dp[i - 1] + dp[i - 2]
+        for i in range(1, len(s)):
+            cur = int(s[i])
+            pre = int(s[i - 1])
+            num = pre * 10 + cur
+            if (pre == 0 and cur == 0 or (cur == 0 and num > 26)):
+                return 0
+            elif (pre == 0 or num > 26):
+                dp[i + 1] = dp[i]
+            elif (cur == 0):
+                dp[i + 1] = dp[i - 1]
             else:
-                dp[i] = dp[i - 1]
+                dp[i + 1] = dp[i] + dp[i - 1]
         return dp[-1]
