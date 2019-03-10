ffbf82b82b318bfd207f741f7a2679e410652ee1
zhongyongbin <zhongyongbin@foxmail.com>
2019-03-10 22:57:20 +0800

    完成

--- algorithms/count-complete-tree-nodes/src/Solution.py
+++ algorithms/count-complete-tree-nodes/src/Solution.py
@@ -8,4 +8,20 @@ class TreeNode:
 
 class Solution:
     def countNodes(self, root: 'TreeNode') -> 'int':
-        pass
+        def count(node):
+            left_hight = 0
+            right_hight = 0
+            left_node = node
+            right_node = node
+            while left_node:
+                left_hight += 1
+                left_node = left_node.left
+            while right_node:
+                right_hight += 1
+                right_node = right_node.right
+            if left_hight == right_hight:
+                return 2**left_hight - 1
+            else:
+                return count(node.left) + count(node.right) + 1
+
+        return count(root)
