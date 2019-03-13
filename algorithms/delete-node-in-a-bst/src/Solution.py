a0742088c9b5abd592bbaf4a2324d0369da26ff5
zhongyongbin <zhongyongbin@foxmail.com>
2019-03-13 11:40:39 +0800

    update

--- algorithms/delete-node-in-a-bst/src/Solution.py
+++ algorithms/delete-node-in-a-bst/src/Solution.py
@@ -8,4 +8,20 @@ class TreeNode:
 
 class Solution:
     def deleteNode(self, root: 'TreeNode', key: 'int') -> 'TreeNode':
-        pass
+        if root is None:
+            return None
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.left and root.right:
+                root.val = self.findMin(root.right).val
+                root.right = self.deleteNode(root.right, root.val)
+            else:
+                root = root.left if root.left else root.right
+
+        return root
+
+    def findMin(self, root):
+        return self.findMin(root.left) if root.left else root
