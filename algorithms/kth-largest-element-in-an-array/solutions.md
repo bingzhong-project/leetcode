ce4d561edfef70d13b266585f34a10eb78443a6b
zhongyongbin <zhongyongbin@foxmail.com>
2019-03-18 11:28:43 +0800

    完成

--- algorithms/kth-largest-element-in-an-array/solutions.md
+++ algorithms/kth-largest-element-in-an-array/solutions.md
@@ -1,3 +1,12 @@
 # Kth Largest Element in an Array
 
 [问题描述](https://leetcode.com/problems/kth-largest-element-in-an-array/)
+
+## 知识点
+
+堆
+
+## 解题思路
+
+本题是典型的一道优先队列应用题，将数组依次添加到优先队列（数值越大优先度越高）中，然后逐个出队即可。  
+很多语言都有现成的优先队列实现，但是使用现成的优先队列的话这题目就没什么难度和意义了，所以自己利用堆构建出优先队列实现。
--- algorithms/kth-largest-element-in-an-array/src/Solution.py
+++ algorithms/kth-largest-element-in-an-array/src/Solution.py
@@ -1,3 +1,34 @@
 class Solution:
     def findKthLargest(self, nums: 'List[int]', k: 'int') -> 'int':
-        pass
+        def heapify(array, hsize, i):
+            left = 2 * i + 1
+            right = 2 * i + 2
+            largest = i
+            if left < hsize and array[left] > array[largest]:
+                largest = left
+            if right < hsize and array[right] > array[largest]:
+                largest = right
+            if largest != i:
+                tmp = array[i]
+                array[i] = array[largest]
+                array[largest] = tmp
+                heapify(array, hsize, largest)
+
+        def build_max_heap(array):
+            for i in range(len(array) // 2, -1, -1):
+                heapify(array, len(array), i)
+
+        def remove_max(array, hsize):
+            max = array[0]
+            array[0] = array[hsize - 1]
+            heapify(array, hsize - 1, 0)
+            return max
+
+        build_max_heap(nums)
+        hsize = len(nums)
+        res = None
+        for i in range(k):
+            res = remove_max(nums, hsize)
+            hsize -= 1
+
+        return res
