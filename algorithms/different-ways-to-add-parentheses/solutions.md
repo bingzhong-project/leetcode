5fa1227c0a0c7ccc9c7baf0aec37b95984effd3d
zhongyongbin <zhongyongbin@foxmail.com>
2019-03-25 11:33:51 +0800

    完成

--- algorithms/different-ways-to-add-parentheses/solutions.md
+++ algorithms/different-ways-to-add-parentheses/solutions.md
@@ -1,3 +1,11 @@
 # Different Ways to Add Parentheses
 
 [问题描述](https://leetcode.com/problems/different-ways-to-add-parentheses/)
+
+## 知识点
+
+分而治之，动态规划
+
+## 解题思路
+
+思路为自顶向下的动态规划思路，递归的计算各部分的算数结果，然后再进行总的运算，得出结果。与题目 [Unique Binary Search Trees II](https://leetcode.com/problems/unique-binary-search-trees-ii/description/) 类似。
--- algorithms/different-ways-to-add-parentheses/src/Solution.py
+++ algorithms/different-ways-to-add-parentheses/src/Solution.py
@@ -1,3 +1,19 @@
 class Solution:
     def diffWaysToCompute(self, input: 'str') -> 'List[int]':
-        pass
+        operator = {'+', '-', '*'}
+        res = []
+        for i in range(len(input)):
+            if input[i] in operator:
+                part1 = self.diffWaysToCompute(input[:i])
+                part2 = self.diffWaysToCompute(input[i + 1:])
+                for p1 in part1:
+                    for p2 in part2:
+                        if input[i] == '+':
+                            res.append(p1 + p2)
+                        elif input[i] == '-':
+                            res.append(p1 - p2)
+                        elif input[i] == '*':
+                            res.append(p1 * p2)
+        if len(res) == 0:
+            res.append(int(input))
+        return res
