56759146abcceac44769652cd1c8c640e06ecf15
zhongyongbin <zhongyongbin@foxmail.com>
2019-04-17 14:15:32 +0800

    添加解法

--- algorithms/lexicographical-numbers/src/Solution.py
+++ algorithms/lexicographical-numbers/src/Solution.py
@@ -1,3 +1,19 @@
 class Solution:
     def lexicalOrder(self, n: 'int') -> 'List[int]':
-        pass
+        def helper(num, n, res):
+            if num > n:
+                return
+            res.append(num)
+            index = len(res) - 1
+            num *= 10
+            while num <= n:
+                res.append(num)
+                num *= 10
+            for i in range(len(res) - 1, index, -1):
+                for j in range(1, 10):
+                    helper(res[i] + j, n, res)
+
+        res = []
+        for i in range(1, 10):
+            helper(i, n, res)
+        return res
