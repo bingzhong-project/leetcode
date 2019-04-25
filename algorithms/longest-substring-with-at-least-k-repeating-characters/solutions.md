8858fb6c6757d937fbffc4d6e9206ec58d6bb4c2
zhongyongbin <zhongyongbin@foxmail.com>
2019-04-25 15:15:24 +0800

    完成

--- algorithms/longest-substring-with-at-least-k-repeating-characters/solutions.md
+++ algorithms/longest-substring-with-at-least-k-repeating-characters/solutions.md
@@ -1,3 +1,12 @@
 # Longest Substring with At Least K Repeating Characters
 
 [问题描述](https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/)
+
+## 知识点
+
+分而治之
+
+## 解题思路
+
+首先对字符串进行字符的统计，然后查看哪些字符出现的次数未够 k 次。未够 k 次的字符肯定不会出现在符合要求的最长子字符串之中，所以以这个字符为切割点，将字符串切割成多个子串，然后子串分别递归重复执行上述过程。  
+当得到的一个满足要求的子串时，返回给长度，然后与其他长度结果一起比较，得出最长长度，即为答案。
--- algorithms/longest-substring-with-at-least-k-repeating-characters/src/Solution.py
+++ algorithms/longest-substring-with-at-least-k-repeating-characters/src/Solution.py
@@ -1,25 +1,15 @@
+import collections
+
+
 class Solution:
     def longestSubstring(self, s: 'str', k: 'int') -> 'int':
-        """分治算法，TLE
-
-        Returns:
-            int: 结果
-        """
-
         def longest(string, k):
             if len(string) < k:
                 return 0
-            char_counter = [0 for _ in range(26)]
-            for e in string:
-                char_counter[ord(e) - ord('a')] += 1
-            for c in range(26):
-                if char_counter[c] < k and char_counter[c] > 0:
-                    for i in range(len(string)):
-                        if ord(string[i]) - ord('a') == c:
-                            left_len = longest(string[:i], k)
-                            right_len = longest(string[i + 1:], k)
-                            return max(left_len, right_len)
-
+            char_counter = collections.Counter(string)
+            for c in char_counter:
+                if char_counter[c] < k:
+                    return max(longest(sub, k) for sub in string.split(c))
             return len(string)
 
         return longest(s, k)
