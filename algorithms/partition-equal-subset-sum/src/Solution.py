c5ed243890eff463bf40ab11a0f71fc3d9accc11
zhongyongbin <zhongyongbin@foxmail.com>
2019-05-06 16:36:07 +0800

    update

--- algorithms/partition-equal-subset-sum/src/Solution.py
+++ algorithms/partition-equal-subset-sum/src/Solution.py
@@ -1,3 +1,23 @@
 class Solution:
     def canPartition(self, nums: 'List[int]') -> 'bool':
-        pass
+        """回溯算法 TLE
+
+        Returns:
+            bool: 答案
+        """
+
+        def func(nums, elements, index, visited, res):
+            amount = sum(elements)
+            if amount == sum(nums) - amount:
+                res[0] = True
+                return
+            for i in range(index, len(nums)):
+                new_elements = elements + [nums[i]]
+                amount = sum(new_elements)
+                if amount not in visited:
+                    visited.add(amount)
+                    func(nums, new_elements, i + 1, visited, res)
+
+        res = [False]
+        func(nums, [], 0, set(), res)
+        return res[0]
