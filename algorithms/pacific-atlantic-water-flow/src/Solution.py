8c5ce39a91e15446cd23d07504582f6824b94b10
zhongyongbin <zhongyongbin@foxmail.com>
2019-05-07 19:36:10 +0800

    update

--- algorithms/pacific-atlantic-water-flow/src/Solution.py
+++ algorithms/pacific-atlantic-water-flow/src/Solution.py
@@ -4,4 +4,27 @@ class Solution:
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
         """
-        pass
+
+        def pacific(i, j):
+            return [(i - 1, j), (i, j - 1)]
+
+        def atlantic(i, j):
+            return [(i + 1, j), (i, j + 1)]
+
+        def dfs(matrix, i, j, visited):
+            pass
+
+        res = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                visited = set()
+                visited.add((i, j))
+                pass
+
+        return res
+
+
+if __name__ == "__main__":
+    print(Solution().pacificAtlantic([[1, 2, 2, 3, 5], [3, 2, 3, 4, 4],
+                                      [2, 4, 5, 3, 1], [6, 7, 1, 4,
+                                                        5], [5, 1, 1, 2, 4]]))
