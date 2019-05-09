902c6998b0a9b2b952a8f1ce81d2793329cd897e
zhongyongbin <zhongyongbin@foxmail.com>
2019-05-09 15:31:05 +0800

    update

--- algorithms/pacific-atlantic-water-flow/src/Solution.py
+++ algorithms/pacific-atlantic-water-flow/src/Solution.py
@@ -4,27 +4,61 @@ class Solution:
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
         """
+        PACIFIC = 1
+        ATLANTIC = 0
+        NOT = -1
+        PACIFIC_ATLANTIC = 10
 
-        def pacific(i, j):
-            return [(i - 1, j), (i, j - 1)]
+        def next(i, j):
+            return [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
 
-        def atlantic(i, j):
-            return [(i + 1, j), (i, j + 1)]
-
-        def dfs(matrix, i, j, visited):
-            pass
+        def dfs(matrix, i, j, visited, cache):
+            if (i, j) in cache:
+                return cache[(i, j)]
+            states = [NOT for _ in range(4)]
+            next_pos = next(i, j)
+            for n in range(len(next_pos)):
+                ni, nj = next_pos[n]
+                if ni < 0:
+                    states[n] = PACIFIC
+                elif ni > len(matrix) - 1:
+                    states[n] = ATLANTIC
+                elif nj < 0:
+                    states[n] = PACIFIC
+                elif nj > len(matrix[0]) - 1:
+                    states[n] = ATLANTIC
+                elif matrix[i][j] >= matrix[ni][nj] and (ni,
+                                                         nj) not in visited:
+                    visited.add((ni, nj))
+                    states[n] = dfs(matrix, ni, nj, visited, cache)
+            is_pacific = False
+            is_atlantic = False
+            for state in states:
+                if state == PACIFIC_ATLANTIC:
+                    is_pacific = True
+                    is_atlantic = True
+                elif state == PACIFIC:
+                    is_pacific = True
+                elif state == ATLANTIC:
+                    is_atlantic = True
+            if is_pacific and is_atlantic:
+                return PACIFIC_ATLANTIC
+            elif is_pacific:
+                return PACIFIC
+            elif is_atlantic:
+                return ATLANTIC
+            else:
+                return NOT
 
         res = []
+        cache = {}
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 visited = set()
                 visited.add((i, j))
-                pass
+                state = dfs(matrix, i, j, visited, cache)
+                cache[(i, j)] = state
+                if state == PACIFIC_ATLANTIC:
+                    res.append([i, j])
 
         return res
-
-
-if __name__ == "__main__":
-    print(Solution().pacificAtlantic([[1, 2, 2, 3, 5], [3, 2, 3, 4, 4],
-                                      [2, 4, 5, 3, 1], [6, 7, 1, 4,
-                                                        5], [5, 1, 1, 2, 4]]))
