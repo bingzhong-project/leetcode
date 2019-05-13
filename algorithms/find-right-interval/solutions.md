0cfdefc70fc8e24a65df8d1e5b55b31f305ab598
zhongyongbin <zhongyongbin@foxmail.com>
2019-05-13 17:46:35 +0800

    完成

--- algorithms/find-right-interval/solutions.md
+++ algorithms/find-right-interval/solutions.md
@@ -1,1 +1,5 @@
 # [Find Right Interval](https://leetcode.com/problems/find-right-interval/)
+
+## 知识点
+
+二分查找
--- algorithms/find-right-interval/src/Solution.py
+++ algorithms/find-right-interval/src/Solution.py
@@ -1,3 +1,25 @@
 class Solution:
     def findRightInterval(self, intervals: 'List[List[int]]') -> 'List[int]':
-        pass
+        def search(prefix, num, left, right):
+            if right - left == 1:
+                return prefix[left][1] if prefix[left][0] >= num else prefix[
+                    right][1]
+            mid = (left + right) // 2
+            if prefix[mid][0] > num:
+                return search(prefix, num, left, mid)
+            elif prefix[mid][0] < num:
+                return search(prefix, num, mid, right)
+            else:
+                return prefix[mid][1]
+
+        prefix = []
+        for i in range(len(intervals)):
+            prefix.append((intervals[i][0], i))
+        prefix.sort()
+
+        res = [-1 for _ in range(len(intervals))]
+        for i in range(len(intervals)):
+            suffix = intervals[i][1]
+            if suffix <= prefix[-1][0]:
+                res[i] = search(prefix, suffix, 0, len(prefix) - 1)
+        return res
