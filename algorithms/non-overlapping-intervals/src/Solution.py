e0066c82e4a0be3ba17fa5963463ad498dc49547
zhongyongbin <zhongyongbin@foxmail.com>
2019-05-14 13:56:29 +0800

    完成

--- algorithms/non-overlapping-intervals/src/Solution.py
+++ algorithms/non-overlapping-intervals/src/Solution.py
@@ -1,3 +1,23 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: 'List[List[int]]') -> 'int':
-        pass
+        if len(intervals) <= 1:
+            return 0
+        intervals.sort(key=lambda interval: interval[0])
+        ans = 0
+        left = 0
+        right = 1
+        while right < len(intervals):
+            left_interval = intervals[left]
+            right_interval = intervals[right]
+            if left_interval[0] == right_interval[0]:
+                ans += 1
+                if left_interval[1] > right_interval[1]:
+                    left = right
+            elif left_interval[1] > right_interval[0]:
+                ans += 1
+                if left_interval[1] > right_interval[1]:
+                    left = right
+            else:
+                left = right
+            right += 1
+        return ans
