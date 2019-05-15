5b20ff1afce690171c019481fe6bd5a8f156bac8
zhongyongbin <zhongyongbin@foxmail.com>
2019-05-15 11:27:44 +0800

    update

--- algorithms/minimum-genetic-mutation/solutions.md
+++ algorithms/minimum-genetic-mutation/solutions.md
@@ -1,1 +1,9 @@
 # [Minimum Genetic Mutation](https://leetcode.com/problems/minimum-genetic-mutation/)
+
+## 知识点
+
+广度优先搜索
+
+## 解题思路
+
+将问题转换成图，每个基因转变成另一个基因即是结点的连通。而题目求的是变换到目标基因的最少次数，其实就是求单源结点的最短路径，而图是无权的，利用广度优先搜索进行计算求解即可。
--- algorithms/minimum-genetic-mutation/src/Solution.py
+++ algorithms/minimum-genetic-mutation/src/Solution.py
@@ -1,4 +1,33 @@
 class Solution:
     def minMutation(self, start: 'str', end: 'str',
                     bank: 'List[str]') -> 'int':
-        pass
+        def conversion(source, target):
+            if len(source) != len(target):
+                return False
+            diff = 0
+            for i in range(len(source)):
+                if source[i] != target[i]:
+                    diff += 1
+                if diff > 1:
+                    break
+            return diff == 1
+
+        bank = set(bank)
+        if end not in bank:
+            return -1
+        ans = 0
+        queue = []
+        queue.append(start)
+        while queue:
+            ans += 1
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                remove = set()
+                for next_node in bank:
+                    if conversion(node, next_node):
+                        if next_node == end:
+                            return ans
+                        remove.add(next_node)
+                        queue.append(next_node)
+                bank -= remove
+        return -1
