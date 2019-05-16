4cbfc561765152a63e15e846e4b24a2898111dc8
zhongyongbin <zhongyongbin@foxmail.com>
2019-05-16 18:09:33 +0800

    完成题目

--- algorithms/flatten-a-multilevel-doubly-linked-list/solutions.md
+++ algorithms/flatten-a-multilevel-doubly-linked-list/solutions.md
@@ -1,1 +1,5 @@
 # [Flatten a Multilevel Doubly Linked List](https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/)
+
+## 知识点
+
+链表，递归
--- algorithms/flatten-a-multilevel-doubly-linked-list/src/Solution.py
+++ algorithms/flatten-a-multilevel-doubly-linked-list/src/Solution.py
@@ -13,4 +13,26 @@ class Solution:
         :type head: Node
         :rtype: Node
         """
-        pass
+        if head is None:
+            return None
+
+        sentinel = Node(None, None, None, None)
+        sentinel.child = head
+        node = head
+        while node:
+            next_node = node.next
+            if node.child:
+                new_node = self.flatten(node.child)
+                node.child = None
+                next_node = node.next
+                node.next = new_node
+                new_node.prev = node
+                last_node = new_node
+                if next_node:
+                    while last_node:
+                        next_node.prev = last_node
+                        last_node = last_node.next
+                    next_node.prev.next = next_node
+            node = next_node
+
+        return sentinel.child
