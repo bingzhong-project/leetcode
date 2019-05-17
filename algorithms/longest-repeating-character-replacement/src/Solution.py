5f50ecbc7aae696ccec3111114f7bace43674ad3
zhongyongbin <zhongyongbin@foxmail.com>
2019-05-17 17:10:47 +0800

    更新

--- algorithms/longest-repeating-character-replacement/src/Solution.py
+++ algorithms/longest-repeating-character-replacement/src/Solution.py
@@ -1,3 +1,23 @@
 class Solution:
     def characterReplacement(self, s: 'str', k: 'int') -> 'int':
-        pass
+        """TLE
+        """
+        res = 0
+        for start in range(len(s)):
+            end = start + 1
+            remain = k
+            count = 1
+            while end <= len(s) - 1:
+                if s[start] == s[end]:
+                    count += 1
+                elif remain > 0:
+                    count += 1
+                    remain -= 1
+                else:
+                    break
+                end += 1
+            if remain > 0:
+                count += remain
+                count = count if count < len(s) else len(s)
+            res = max(count, res)
+        return res
