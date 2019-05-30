3bcdceed9edb5d4cb09fd2a21e960bd7786d425d
zhongyongbin <zhongyongbin@foxmail.com>
2019-05-30 18:12:54 +0800

    完成

--- algorithms/unique-substrings-in-wraparound-string/solutions.md
+++ algorithms/unique-substrings-in-wraparound-string/solutions.md
@@ -1,1 +1,9 @@
 # [Unique Substrings in Wraparound String](https://leetcode.com/problems/unique-substrings-in-wraparound-string/)
+
+## 知识点
+
+动态规划
+
+## 解题思路
+
+参考：[Unique Substrings in Wraparound String Solution](https://www.cnblogs.com/grandyang/p/6143071.html)
--- algorithms/unique-substrings-in-wraparound-string/src/Solution.py
+++ algorithms/unique-substrings-in-wraparound-string/src/Solution.py
@@ -1,3 +1,25 @@
+import collections
+
+
 class Solution:
     def findSubstringInWraproundString(self, p: 'str') -> 'int':
-        pass
+        def validate(pre, cur):
+            last_ord = ord(pre)
+            cur_ord = ord(cur)
+            if last_ord == ord('z') and cur_ord == ord('a'):
+                return True
+            else:
+                return cur_ord == last_ord + 1
+
+        if len(p) == 0:
+            return 0
+        dp = collections.Counter()
+        length = 1
+        dp[p[0]] = 1
+        for i in range(1, len(p)):
+            if validate(p[i - 1], p[i]):
+                length += 1
+            else:
+                length = 1
+            dp[p[i]] = max(dp[p[i]], length)
+        return sum(dp.values())
