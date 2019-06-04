e5bffd3e3d8ea68f5c913791ad748284c65b0d6c
zhongyongbin <zhongyongbin@foxmail.com>
2019-06-04 10:55:39 +0800

    更新思路

--- algorithms/ones-and-zeroes/solutions.md
+++ algorithms/ones-and-zeroes/solutions.md
@@ -3,3 +3,10 @@
 ## 知识点
 
 动态规划
+
+## 解题思路
+
+对于动态规划题目，首先要分析出状态的变化流程，从而推导出状态转移方程。  
+对于该题目，对于给定的字符串数组，每个元素都有选择与不选择两个选项，选择元素，则 m 和 n 减去对应的 0 数量和 1 数量，当前的状态变化为 Y 。而不选择的话，m 和 n 的数量将不变化，当前状态为 N ，接下来就是比较 Y 和 N 状态中，谁的结果较大，使之作为最终选择的最优状态。
+
+参考资料：[Ones and Zeroes Solution](https://leetcode.com/problems/ones-and-zeroes/discuss/261086/Python-EASY-to-understand-with-explanation-beat-98-top-down-and-bottom-up)
--- algorithms/ones-and-zeroes/src/Solution2.py
+++ algorithms/ones-and-zeroes/src/Solution2.py
@@ -1,11 +1,16 @@
+import collections
+
+
 class Solution:
     def findMaxForm(self, strs: 'List[str]', m: 'int', n: 'int') -> 'int':
-        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        dp = collections.Counter()
+        dp[(m, n)] = 0
         for s in strs:
             zeros = s.count('0')
             ones = len(s) - zeros
-            for i in range(m, zeros - 1, -1):
-                for j in range(n, ones - 1, -1):
-                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
+            for (dm, dn), count in dp.copy().items():
+                if dm - zeros >= 0 and dn - ones >= 0:
+                    dp[(dm - zeros, dn - ones)] = max(
+                        dp[(dm - zeros, dn - ones)], count + 1)
 
-        return dp[-1][-1]
+        return max(dp.values())
