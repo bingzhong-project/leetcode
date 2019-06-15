e2b76fb3537b77a8c5452749981fc0fe728e521c
zhongyongbin <zhongyongbin@foxmail.com>
2019-06-15 11:09:21 +0800

    更新

--- algorithms/longest-palindromic-subsequence/src/Solution.py
+++ algorithms/longest-palindromic-subsequence/src/Solution.py
@@ -1,3 +1,17 @@
 class Solution:
     def longestPalindromeSubseq(self, s: 'str') -> 'int':
-        pass
+        """TLE
+        """
+        dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = 1
+        for j in range(len(s)):
+            for i in range(j):
+                if s[i] == s[j]:
+                    if j - i == 1:
+                        dp[i][j] = 2
+                    else:
+                        dp[i][j] = 2 + max(max(row) for row in dp[i + 1:][:j])
+                else:
+                    dp[i][j] = max(dp[i])
+        return max(max(row) for row in dp)
