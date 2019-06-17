fcd3bf1c875ac94e94e7e50cf5745abe355cee18
zhongyongbin <zhongyongbin@foxmail.com>
2019-06-17 16:49:21 +0800

    完成

--- algorithms/coin-change-2/solutions.md
+++ algorithms/coin-change-2/solutions.md
@@ -1,1 +1,5 @@
 # [Coin Change 2](https://leetcode.com/problems/coin-change-2/)
+
+## 知识点
+
+动态规划
--- algorithms/coin-change-2/src/Solution.py
+++ algorithms/coin-change-2/src/Solution.py
@@ -1,3 +1,19 @@
 class Solution:
     def change(self, amount: 'int', coins: 'List[int]') -> 'int':
-        pass
+        def func(amount, coins, index, memo):
+            if (index, amount) in memo:
+                return memo[(index, amount)]
+            if amount == 0:
+                return 1
+            ways = 0
+            for i in range(index, -1, -1):
+                if amount >= coins[i]:
+                    ways += func(amount - coins[i], coins, i, memo)
+            memo[(index, amount)] = ways
+            return ways
+
+        return func(amount, coins, len(coins) - 1, {})
+
+
+if __name__ == "__main__":
+    print(Solution().change(5, [1, 2, 5]))
