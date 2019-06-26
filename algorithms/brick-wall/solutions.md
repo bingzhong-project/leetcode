1364269de1fb0e28ab19fbd8274b1b36baa52aaf
zhongyongbin <zhongyongbin@foxmail.com>
2019-06-26 18:43:09 +0800

    完成

--- algorithms/brick-wall/solutions.md
+++ algorithms/brick-wall/solutions.md
@@ -1,1 +1,5 @@
 # [Brick Wall](https://leetcode.com/problems/brick-wall/)
+
+## 知识点
+
+累加和，哈希表
--- algorithms/brick-wall/src/Solution.py
+++ algorithms/brick-wall/src/Solution.py
@@ -1,3 +1,15 @@
+import collections
+
+
 class Solution:
     def leastBricks(self, wall: 'List[List[int]]') -> 'int':
-        pass
+        wall_length = sum(wall[0])
+        counter = collections.Counter()
+        for w in wall:
+            ws = 0
+            for i in range(len(w)):
+                ws += w[i]
+                if ws != wall_length:
+                    counter[ws] += 1
+        return len(wall) - max(list(
+            counter.values())) if counter else len(wall)
