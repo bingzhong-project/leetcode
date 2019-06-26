f84603bc7b848f9881a5b8a7b3234a37e1aeba7d
zhongyongbin <zhongyongbin@foxmail.com>
2019-06-26 18:45:00 +0800

    update

--- algorithms/brick-wall/src/Solution.py
+++ algorithms/brick-wall/src/Solution.py
@@ -5,11 +5,11 @@ class Solution:
     def leastBricks(self, wall: 'List[List[int]]') -> 'int':
         wall_length = sum(wall[0])
         counter = collections.Counter()
+        counter[0] = 0
         for w in wall:
             ws = 0
             for i in range(len(w)):
                 ws += w[i]
                 if ws != wall_length:
                     counter[ws] += 1
-        return len(wall) - max(list(
-            counter.values())) if counter else len(wall)
+        return len(wall) - max(list(counter.values()))
