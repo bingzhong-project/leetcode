d349deeadce303815d1dde6de84db6d63ff19f96
zhongyongbin <zhongyongbin@foxmail.com>
2019-07-01 18:43:08 +0800

    完成

--- algorithms/subarray-sum-equals-k/solutions.md
+++ algorithms/subarray-sum-equals-k/solutions.md
@@ -1,1 +1,5 @@
 # [Subarray Sum Equals K](https://leetcode.com/problems/subarray-sum-equals-k/)
+
+## 知识点
+
+子数组累计和，哈希表
--- algorithms/subarray-sum-equals-k/src/Solution.py
+++ algorithms/subarray-sum-equals-k/src/Solution.py
@@ -1,3 +1,15 @@
+import collections
+
+
 class Solution:
     def subarraySum(self, nums: 'List[int]', k: 'int') -> 'int':
-        pass
+        table = collections.Counter()
+        table[0] = 1
+        s = 0
+        res = 0
+        for i in range(len(nums)):
+            s += nums[i]
+            if s - k in table:
+                res += table[s - k]
+            table[s] += 1
+        return res
