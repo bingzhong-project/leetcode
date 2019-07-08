447a7ef604f241e4e458bea985f60f69d617785a
zhongyongbin <zhongyongbin@foxmail.com>
2019-07-08 23:51:24 +0800

    更新

--- algorithms/permutation-in-string/src/Solution.py
+++ algorithms/permutation-in-string/src/Solution.py
@@ -3,35 +3,40 @@ import collections
 
 class Solution:
     def checkInclusion(self, s1: 'str', s2: 'str') -> 'bool':
-        """TLE
-        """
         counter = collections.Counter()
+        keys = set()
         for s in s1:
             counter[s] += 1
-        backup = counter.copy()
+            keys.add(s)
         left = 0
-        right = len(s1)
-        while right <= len(s2):
-            for i in range(left, right):
-                s = s2[i]
-                if s in counter:
-                    counter[s] -= 1
-                    if counter[s] == 0:
-                        del counter[s]
-                    if len(counter) == 0:
-                        return True
-                else:
-                    break
-            counter = backup.copy()
-            left += 1
+        right = 0
+        length = len(counter)
+        while right < len(s2):
+            s = s2[right]
+            if s in counter and counter[s] > 0:
+                counter[s] -= 1
+            else:
+                if s2[left] in keys:
+                    counter[s2[left]] += 1
+                    length += 1
+                left += 1
+            if counter[s] == 0:
+                length -= 1
+            if length == 0:
+                return True
             right += 1
 
-        return len(counter) == 0
+        return length == 0
 
 
 if __name__ == "__main__":
     print(Solution().checkInclusion(s1="adc", s2="dcda"))
-    print(Solution().checkInclusion(s1="ab", s2="eidbaooo"))
-    print(Solution().checkInclusion(s1="ab", s2="eidboaoo"))
-    print(Solution().checkInclusion(s1="ccc", s2="cbac"))
-    print(Solution().checkInclusion(s1="a", s2="a"))
+    # print(Solution().checkInclusion(s1="ab", s2="eidbaooo"))
+    # print(Solution().checkInclusion(s1="ab", s2="eidboaoo"))
+    # print(Solution().checkInclusion(s1="ccc", s2="cbac"))
+    # print(Solution().checkInclusion(s1="a", s2="a"))
+    # print(Solution().checkInclusion("pqzhi", "ghrqpihzybre"))
+    # print(Solution().checkInclusion(
+    # "trinitrophenylmethylnitramine",
+    # "dinitrophenylhydrazinetrinitrophenylmethylnitramine"))
+    print(Solution().checkInclusion("hello", "ooolleoooleh"))
