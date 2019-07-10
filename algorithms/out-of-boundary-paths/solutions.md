02421bc55311aad39c7272560c4e5da8ff0458fb
zhongyongbin <zhongyongbin@foxmail.com>
2019-07-10 21:17:42 +0800

    完成

--- algorithms/out-of-boundary-paths/solutions.md
+++ algorithms/out-of-boundary-paths/solutions.md
@@ -1,1 +1,5 @@
-# [Out Of Boundary Paths](https://leetcode-cn.com/problems/out-of-boundary-paths/)
+# [Out Of Boundary Paths](https://leetcode.com/problems/out-of-boundary-paths/)
+
+## 知识点
+
+动态规划，深度优先搜索
--- algorithms/out-of-boundary-paths/src/Solution.py
+++ algorithms/out-of-boundary-paths/src/Solution.py
@@ -1,4 +1,20 @@
 class Solution:
     def findPaths(self, m: 'int', n: 'int', N: 'int', i: 'int',
                   j: 'int') -> 'int':
-        pass
+        def func(m, n, N, i, j, memo):
+            if (i, j, N) in memo:
+                return memo[(i, j, N)]
+            if i < 0 or j >= n or i >= m or j < 0:
+                return 1
+            if N == 0:
+                return 0
+            left_count = func(m, n, N - 1, i - 1, j, memo)
+            right_count = func(m, n, N - 1, i + 1, j, memo)
+            up_count = func(m, n, N - 1, i, j - 1, memo)
+            down_count = func(m, n, N - 1, i, j + 1, memo)
+            total_count = left_count + right_count + up_count + down_count
+            memo[(i, j, N)] = total_count
+            return total_count
+
+        res = func(m, n, N, i, j, {})
+        return res % (10**9 + 7)
