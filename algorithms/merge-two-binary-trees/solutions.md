ea3c1e88a564c4ccfefa9f3d9c926eccf43dc99f
zhongyongbin <zhongyongbin@foxmail.com>
2019-07-18 20:31:29 +0800

    完成

--- algorithms/merge-two-binary-trees/solutions.md
+++ algorithms/merge-two-binary-trees/solutions.md
@@ -1,1 +1,5 @@
 # [Merge Two Binary Trees](https://leetcode.com/problems/merge-two-binary-trees/)
+
+## 知识点
+
+树的遍历
--- algorithms/merge-two-binary-trees/src/Solution.py
+++ algorithms/merge-two-binary-trees/src/Solution.py
@@ -8,4 +8,14 @@ class TreeNode:
 
 class Solution:
     def mergeTrees(self, t1: 'TreeNode', t2: 'TreeNode') -> 'TreeNode':
-        pass
+        def dfs(t1, t2):
+            if t1 is None and t2 is None:
+                return None
+            val1 = 0 if t1 is None else t1.val
+            val2 = 0 if t2 is None else t2.val
+            node = TreeNode(val1 + val2)
+            node.left = dfs(t1.left if t1 else None, t2.left if t2 else None)
+            node.right = dfs(t1.right if t1 else None, t2.right if t2 else None)
+            return node
+
+        return dfs(t1, t2)
