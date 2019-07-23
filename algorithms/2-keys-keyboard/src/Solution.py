5e36d0d2b688e5de7ed68e5d32495b23ff381689
zhongyongbin <zhongyongbin@foxmail.com>
2019-07-23 11:39:16 +0800

    完成

--- algorithms/2-keys-keyboard/src/Solution.py
+++ algorithms/2-keys-keyboard/src/Solution.py
@@ -1,3 +1,18 @@
 class Solution:
     def minSteps(self, n: 'int') -> 'int':
-        pass
+        def find_factor(num):
+            for i in range(2, num // 2 + 1):
+                div, mod = divmod(num, i)
+                if mod == 0:
+                    return [i, div]
+            return [1, num]
+
+        dp = [0 for _ in range(n + 1)]
+        for i in range(2, n + 1):
+            factor = find_factor(i)
+            step = 0
+            for num in factor:
+                step += (dp[num] if num == 1 or dp[num] > 0 else num)
+            dp[i] = step
+
+        return dp[-1]
