03bcb11cc534644412f80248837d9fb93a902af5
zhongyongbin <zhongyongbin@foxmail.com>
2019-07-26 19:05:11 +0800

    完成

--- algorithms/maximum-length-of-pair-chain/src/Solution.py
+++ algorithms/maximum-length-of-pair-chain/src/Solution.py
@@ -1,3 +1,13 @@
 class Solution:
     def findLongestChain(self, pairs: 'List[List[int]]') -> 'int':
-        pass
+        pairs.sort(key=lambda x: x[0])
+        res = 1
+        cur = pairs[0][1]
+        for i in range(1, len(pairs)):
+            pair = pairs[i]
+            if cur >= pair[0]:
+                cur = min(cur, pair[1])
+            else:
+                cur = pair[1]
+                res += 1
+        return res
