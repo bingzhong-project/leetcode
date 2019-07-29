f49f02bde9df2c7b751904e052b5c0c11b980242
zhongyongbin <zhongyongbin@foxmail.com>
2019-07-29 20:21:49 +0800

    完成

--- algorithms/two-sum-iv-input-is-a-bst/src/Solution.py
+++ algorithms/two-sum-iv-input-is-a-bst/src/Solution.py
@@ -8,4 +8,33 @@ class TreeNode:
 
 class Solution:
     def findTarget(self, root: 'TreeNode', k: 'int') -> 'bool':
-        pass
+        def search(root, target):
+            if root is None:
+                return False
+            if root.val > target:
+                return search(root.left, target)
+            elif root.val < target:
+                return search(root.right, target)
+            else:
+                return True
+
+        def dfs(root, node, k, res):
+            if root is None:
+                return
+            if node is None:
+                return
+            target = k - node.val
+            if target != node.val:
+                if search(root, target):
+                    res[0] = True
+                    return
+                if search(root, target):
+                    res[0] = True
+                    return
+            dfs(root, node.left, k, res)
+            dfs(root, node.right, k, res)
+
+        res = [False]
+        dfs(root, root, k, res)
+
+        return res[0]
