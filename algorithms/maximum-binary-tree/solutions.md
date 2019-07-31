8799b97a10689696ba0d69077254ff0e41e0724e
zhongyongbin <zhongyongbin@foxmail.com>
2019-07-31 20:58:20 +0800

    完成

--- algorithms/maximum-binary-tree/solutions.md
+++ algorithms/maximum-binary-tree/solutions.md
@@ -1,1 +1,5 @@
 # [Maximum Binary Tree](https://leetcode.com/problems/maximum-binary-tree/)
+
+## 解题思路
+
+树
--- algorithms/maximum-binary-tree/src/Solution.py
+++ algorithms/maximum-binary-tree/src/Solution.py
@@ -8,4 +8,14 @@ class TreeNode:
 
 class Solution:
     def constructMaximumBinaryTree(self, nums: 'List[int]') -> 'TreeNode':
-        pass
+        if len(nums) == 0:
+            return None
+        max_val = max(nums)
+        max_index = nums.index(max_val)
+        left_node = self.constructMaximumBinaryTree(nums[:max_index])
+        right_node = self.constructMaximumBinaryTree(nums[max_index + 1:])
+        root = TreeNode(max_val)
+        root.left = left_node
+        root.right = right_node
+
+        return root
