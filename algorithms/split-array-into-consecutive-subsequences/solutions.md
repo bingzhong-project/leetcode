a39bac3d302e8950358d2dd879086b313a5cd192
zhongyongbin <zhongyongbin@foxmail.com>
2019-08-07 19:35:47 +0800

    完成

--- algorithms/split-array-into-consecutive-subsequences/solutions.md
+++ algorithms/split-array-into-consecutive-subsequences/solutions.md
@@ -1,1 +1,9 @@
 # [Split Array into Consecutive Subsequences](https://leetcode.com/problems/split-array-into-consecutive-subsequences/)
+
+## 知识点
+
+贪心算法
+
+## 解题思路
+
+参考：[Split Array into Consecutive Subsequences 解题](https://www.cnblogs.com/grandyang/p/7525821.html)
--- algorithms/split-array-into-consecutive-subsequences/src/Solution.py
+++ algorithms/split-array-into-consecutive-subsequences/src/Solution.py
@@ -1,3 +1,21 @@
+import collections
+
+
 class Solution:
     def isPossible(self, nums: 'List[int]') -> 'bool':
-        pass
+        freq = collections.Counter(nums)
+        need = collections.Counter()
+        for num in nums:
+            if freq[num] == 0:
+                continue
+            if need[num] > 0:
+                need[num] -= 1
+                need[num + 1] += 1
+            elif freq[num + 1] > 0 and freq[num + 2] > 0:
+                freq[num + 1] -= 1
+                freq[num + 2] -= 1
+                need[num + 3] += 1
+            else:
+                return False
+            freq[num] -= 1
+        return True
