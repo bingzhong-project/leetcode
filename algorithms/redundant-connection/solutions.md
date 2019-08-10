422b4aeefca432bd0d00e9568b32b8d0885259b6
zhongyongbin <zhongyongbin@foxmail.com>
2019-08-10 10:45:03 +0800

    完成

--- algorithms/redundant-connection/solutions.md
+++ algorithms/redundant-connection/solutions.md
@@ -1,1 +1,5 @@
 # [Redundant Connection](https://leetcode.com/problems/redundant-connection/)
+
+## 知识点
+
+图，拓扑排序
--- algorithms/redundant-connection/src/Solution.py
+++ algorithms/redundant-connection/src/Solution.py
@@ -1,3 +1,26 @@
 class Solution:
     def findRedundantConnection(self, edges: 'List[List[int]]') -> 'List[int]':
-        pass
+        def topology(adj, color, u, pre):
+            if color[u] == BLACK:
+                return True
+            if color[u] == GRAY:
+                return False
+            color[u] = GRAY
+            for v in adj[u]:
+                if pre == v:
+                    continue
+                if not topology(adj, color, v, u):
+                    return False
+            color[u] = BLACK
+            return True
+
+        WHITE = 0
+        GRAY = -1
+        BLACK = 1
+        adj = [[] for _ in range(len(edges) + 1)]
+        for u, v in tuple(edges):
+            adj[u].append(v)
+            adj[v].append(u)
+            color = [WHITE for _ in range(len(edges) + 1)]
+            if not topology(adj, color, u, None):
+                return [u, v]
