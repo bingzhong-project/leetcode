72d2d4c873619013506af8cfdaad2dcb7fb8dffe
zhongyongbin <zhongyongbin@foxmail.com>
2019-08-14 18:46:12 +0800

    完成

--- algorithms/number-of-longest-increasing-subsequence/solutions.md
+++ algorithms/number-of-longest-increasing-subsequence/solutions.md
@@ -1,1 +1,9 @@
 # [Number of Longest Increasing Subsequence](https://leetcode.com/problems/number-of-longest-increasing-subsequence/)
+
+## 知识点
+
+动态规划
+
+## 解题思路
+
+参考：[Number of Longest Increasing Subsequence 解题](https://www.cnblogs.com/grandyang/p/7603903.html)
--- algorithms/number-of-longest-increasing-subsequence/src/Solution.py
+++ algorithms/number-of-longest-increasing-subsequence/src/Solution.py
@@ -1,3 +1,24 @@
 class Solution:
     def findNumberOfLIS(self, nums: 'List[int]') -> 'int':
-        pass
+        if len(nums) == 0:
+            return 0
+        length = [1 for _ in range(len(nums))]
+        count = [1 for _ in range(len(nums))]
+        max_count = 0
+        max_length = 0
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] >= nums[i]:
+                    continue
+                if length[i] == length[j] + 1:
+                    count[i] += count[j]
+                elif length[i] < length[j] + 1:
+                    length[i] = length[j] + 1
+                    count[i] = count[j]
+            if length[i] == max_length:
+                max_count += count[i]
+            elif max_length < length[i]:
+                max_count = count[i]
+                max_length = length[i]
+
+        return max_count
