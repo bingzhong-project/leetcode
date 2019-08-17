fff2595d43d029b0ff25c0c3eb7d58441743b4f9
zhongyongbin <zhongyongbin@foxmail.com>
2019-08-17 10:27:24 +0800

    完成

--- algorithms/partition-to-k-equal-sum-subsets/solutions.md
+++ algorithms/partition-to-k-equal-sum-subsets/solutions.md
@@ -1,1 +1,9 @@
 # [Partition to K Equal Sum Subsets](https://leetcode.com/problems/partition-to-k-equal-sum-subsets/)
+
+## 知识点
+
+递归
+
+## 解题思路
+
+参考：[Partition to K Equal Sum Subsets 解题](https://www.cnblogs.com/grandyang/p/7733098.html)
--- algorithms/partition-to-k-equal-sum-subsets/src/Solution.py
+++ algorithms/partition-to-k-equal-sum-subsets/src/Solution.py
@@ -1,3 +1,22 @@
 class Solution:
     def canPartitionKSubsets(self, nums: 'List[int]', k: 'int') -> 'bool':
-        pass
+        def func(nums, index, cur_sum, target, k, visited):
+            if k == 1:
+                return True
+            if cur_sum == target:
+                return func(nums, 0, 0, target, k - 1, visited)
+            for i in range(index, len(nums)):
+                if visited[i]:
+                    continue
+                visited[i] = True
+                if (func(nums, i + 1, cur_sum + nums[i], target, k, visited)):
+                    return True
+                visited[i] = False
+
+            return False
+
+        nums_sum = sum(nums)
+        if nums_sum % k != 0:
+            return False
+        target = nums_sum // k
+        return func(nums, 0, 0, target, k, [False for _ in range(len(nums))])
