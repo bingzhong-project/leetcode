e8f5af22bdb16609ec69ba666bf1a97c329ebcc3
zhongyongbin <zhongyongbin@foxmail.com>
2019-08-24 10:28:06 +0800

    完成

--- algorithms/maximum-swap/src/Solution.py
+++ algorithms/maximum-swap/src/Solution.py
@@ -1,3 +1,28 @@
+import collections
+
+
 class Solution:
     def maximumSwap(self, num: 'int') -> 'int':
-        pass
+        array = []
+        while num > 0:
+            div, mod = divmod(num, 10)
+            array.append(mod)
+            num = div
+        change = collections.defaultdict(list)
+        for i in range(len(array)):
+            for j in range(i + 1, len(array)):
+                if array[i] > array[j]:
+                    change[array[i]] += [(i, j)]
+        for i in range(10, 0, -1):
+            if i in change:
+                change[i].sort(key=lambda x: (x[0], -x[1]))
+                m, n = change[i][0]
+                temp = array[m]
+                array[m] = array[n]
+                array[n] = temp
+                break
+
+        res = 0
+        for i in range(len(array)):
+            res += array[i] * 10**i
+        return res
