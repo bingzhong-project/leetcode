51192d84062f3feab6125a01f8de5872e87ea83e
zhongyongbin <zhongyongbin@foxmail.com>
2019-08-25 11:16:16 +0800

    完成

--- algorithms/valid-parenthesis-string/src/Solution.py
+++ algorithms/valid-parenthesis-string/src/Solution.py
@@ -1,3 +1,31 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        pass
+        parenthesis = []
+        stars = []
+        for i in range(len(s)):
+            string = s[i]
+            if string == '*':
+                stars.append((string, i))
+            else:
+                if len(parenthesis) == 0:
+                    if string == ')':
+                        if len(stars) > 0:
+                            stars.pop()
+                        else:
+                            return False
+                    else:
+                        parenthesis.append((string, i))
+                else:
+                    if string == '(':
+                        parenthesis.append((string, i))
+                    else:
+                        parenthesis.pop()
+        while parenthesis:
+            if len(stars) == 0:
+                return False
+            p, pi = parenthesis.pop()
+            s, si = stars.pop()
+            if si < pi:
+                return False
+
+        return True
