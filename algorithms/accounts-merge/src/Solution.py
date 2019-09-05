0583784ffcdca98aa1b57397a3ee302a5878a626
zhongyongbin <zhongyongbin@foxmail.com>
2019-09-05 10:49:49 +0800

    完成

--- algorithms/accounts-merge/src/Solution.py
+++ algorithms/accounts-merge/src/Solution.py
@@ -1,11 +1,31 @@
+import collections
+
+
 class Solution:
     def accountsMerge(self, accounts: list) -> list:
-        pass
+        def dfs(adj, accounts, visited, mail, merge):
+            if mail in visited:
+                return
+            merge.append(mail)
+            visited.add(mail)
+            for i in adj[mail]:
+                for j in range(1, len(accounts[i])):
+                    dfs(adj, accounts, visited, accounts[i][j], merge)
 
+        adj = collections.defaultdict(list)
+        for i in range(len(accounts)):
+            for j in range(1, len(accounts[i])):
+                mail = accounts[i][j]
+                adj[mail] += [i]
+        res = []
+        visited = set()
+        for i in range(len(accounts)):
+            merge = []
+            for j in range(1, len(accounts[i])):
+                if accounts[i][j] in visited:
+                    continue
+                dfs(adj, accounts, visited, accounts[i][j], merge)
+            if len(merge) > 0:
+                res.append([accounts[i][0]] + sorted(merge))
 
-if __name__ == "__main__":
-    print(Solution().accountsMerge(
-        [["John", "johnsmith@mail.com",
-          "john00@mail.com"], ["John", "johnnybravo@mail.com"],
-         ["John", "johnsmith@mail.com",
-          "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]))
+        return res
