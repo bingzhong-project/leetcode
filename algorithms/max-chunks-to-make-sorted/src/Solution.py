84482c3c7ff1a439d1a8ed30e33ab3bab93bef9d
zhongyongbin <zhongyongbin@foxmail.com>
2019-09-27 20:11:33 +0800

    完成

--- algorithms/max-chunks-to-make-sorted/src/Solution.py
+++ algorithms/max-chunks-to-make-sorted/src/Solution.py
@@ -1,3 +1,17 @@
 class Solution:
     def maxChunksToSorted(self, arr: list) -> int:
-        pass
+        res = 0
+        cur = 0
+        i = 0
+        while i < len(arr):
+            cur = arr[i]
+            j = i + 1
+            while j < cur + 1:
+                cur = max(cur, arr[j])
+                if cur == len(arr) - 1:
+                    return res + 1
+                j += 1
+            i = j
+            res += 1
+
+        return res
