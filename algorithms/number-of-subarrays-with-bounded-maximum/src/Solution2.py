fda328e7eee3f08152b12575ff54f23656e404f8
zhongyongbin <zhongyongbin@foxmail.com>
2019-10-08 22:16:59 +0800

    完成

--- algorithms/number-of-subarrays-with-bounded-maximum/src/Solution2.py
+++ algorithms/number-of-subarrays-with-bounded-maximum/src/Solution2.py
@@ -3,9 +3,14 @@ class Solution:
         def count(arr, bound):
             res = 0
             cur = 0
-            for num in arr:
-                cur = cur + 1 if num <= bound else 0
-                res += cur
-            return res
+            start = 0
+            for end in range(len(arr)):
+                if arr[end] <= bound:
+                    cur += end - start + 1
+                else:
+                    res += cur
+                    cur = 0
+                    start = end + 1
+            return res + cur
 
         return count(A, R) - count(A, L - 1)
