e1bbe26a2ca1b4a683d37faf2eea0bcf46871ff3
zhongyongbin <zhongyongbin@foxmail.com>
2019-10-13 19:47:03 +0800

    完成

--- algorithms/pyramid-transition-matrix/src/Solution.py
+++ algorithms/pyramid-transition-matrix/src/Solution.py
@@ -1,3 +1,22 @@
+import collections
+
+
 class Solution:
     def pyramidTransition(self, bottom: str, allowed: list) -> bool:
-        pass
+        def func(cur, above, data):
+            if len(cur) == 2 and len(above) == 1:
+                return True
+            if len(above) == len(cur) - 1:
+                return func(above, "", data)
+            pos = len(above)
+            for string in data[cur[pos] + cur[pos + 1]]:
+                if func(cur, above + string, data):
+                    return True
+
+            return False
+
+        data = collections.defaultdict(list)
+        for a in allowed:
+            data[a[0] + a[1]] += [a[2]]
+
+        return func(bottom, "", data)
