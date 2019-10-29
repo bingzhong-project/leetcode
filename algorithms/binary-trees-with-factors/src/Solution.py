6da4984e4c2d3214fa00807fcf7ab5caa221e585
zhongyongbin <zhongyongbin@foxmail.com>
2019-10-29 22:37:18 +0800

    update

--- algorithms/binary-trees-with-factors/src/Solution.py
+++ algorithms/binary-trees-with-factors/src/Solution.py
@@ -4,11 +4,25 @@ class Solution:
             if num in memo:
                 return memo[num]
             if num not in factor_map:
-                factor_map[num] = 0
+                memo[num] = 0
                 return 0
             factor1, factor2 = factor_map[num]
+            left_sum = func(factor1, factor_map, memo)
+            right_sum = func(factor2, factor_map, memo)
+            cur = left_sum + right_sum + 1
+
+            cur = cur if factor1 == factor2 else cur * 2
+            memo[num] = cur % (10**9 + 7)
+            return memo[num]
 
         factor_map = {}
-        for i in range(A):
-            for j in range(A):
+        for i in range(len(A)):
+            for j in range(len(A)):
                 factor_map[A[i] * A[j]] = (A[i], A[j])
+
+        res = 0
+        memo = {}
+        for num in sorted(A):
+            res += func(num, factor_map, memo)
+
+        return res + len(A)
