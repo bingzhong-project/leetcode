ae39ca9988e76b4f330ae8d0e71b72341739e364
zhongyongbin <zhongyongbin@foxmail.com>
2019-10-30 20:55:19 +0800

    完成

--- algorithms/binary-trees-with-factors/solutions.md
+++ algorithms/binary-trees-with-factors/solutions.md
@@ -1,1 +1,5 @@
 # [Binary Trees With Factors](https://leetcode.com/problems/binary-trees-with-factors/)
+
+## 知识点
+
+动态规划
--- algorithms/binary-trees-with-factors/src/Solution.py
+++ algorithms/binary-trees-with-factors/src/Solution.py
@@ -1,39 +1,18 @@
 class Solution:
     def numFactoredBinaryTrees(self, A: list) -> int:
-        def func(num, factor_map, memo):
-            if num in memo:
-                return memo[num]
-            if num not in factor_map:
-                memo[num] = 0
-                return 0
-            res = 0
-
-            for factor1, factor2 in factor_map[num]:
-                left_count = func(factor1, factor_map, memo)
-                right_count = func(factor2, factor_map, memo)
-                count = left_count + right_count + 1
-                res += count
-
-            memo[num] = res % (10**9 + 7)
-            return memo[num]
-
-        factor_map = {}
+        M = 10**9 + 7
+        dp = {}
+        A.sort()
         for i in range(len(A)):
-            for j in range(len(A)):
-                key = A[i] * A[j]
-                if key not in set(A):
+            dp[A[i]] = 1
+            for j in range(i):
+                div, mod = divmod(A[i], A[j])
+                if mod != 0:
                     continue
-                if key not in factor_map:
-                    factor_map[key] = set()
-                factor_map[key].add((A[i], A[j]))
-
+                if div not in dp:
+                    continue
+                dp[A[i]] = (dp[A[i]] + dp[A[j]] * dp[div]) % M
         res = 0
-        memo = {}
-        for num in A:
-            res += func(num, factor_map, memo)
-
-        return res + len(A)
-
-
-if __name__ == "__main__":
-    print(Solution().numFactoredBinaryTrees([2, 4, 8, 16]))
+        for num in dp.values():
+            res = (res + num) % M
+        return res
