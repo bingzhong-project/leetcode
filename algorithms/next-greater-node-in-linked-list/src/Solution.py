5d3faf5e737f121237a9435108c56e1c1eb3315b
zhongyongbin <zhongyongbin@foxmail.com>
2019-11-26 21:02:21 +0800

    完成

--- algorithms/next-greater-node-in-linked-list/src/Solution.py
+++ algorithms/next-greater-node-in-linked-list/src/Solution.py
@@ -7,8 +7,20 @@ class ListNode:
 
 class Solution:
     def nextLargerNodes(self, head: ListNode) -> list:
+        cache = {}
         stack = []
         node = head
+        index = 0
         while node:
-            while stack and stack[-1] < node.val:
-                stack.pop()
+            while stack and stack[-1][1] < node.val:
+                cache[stack.pop()[0]] = node.val
+            stack.append((index, node.val))
+            node = node.next
+            index += 1
+
+        array = [0 for _ in range(index)]
+        for i in range(len(array)):
+            if i in cache:
+                array[i] = cache[i]
+
+        return array
