167d8c7309b3c575a20e0f70327d4d3fda756d17
zhongyongbin <zhongyongbin@foxmail.com>
2020-01-06 21:24:54 +0800

    完成

--- algorithms/intersection-of-two-linked-lists/src/Solution2.py
+++ algorithms/intersection-of-two-linked-lists/src/Solution2.py
@@ -8,11 +8,28 @@ class ListNode:
 class Solution:
     def getIntersectionNode(self, headA: ListNode,
                             headB: ListNode) -> ListNode:
+        if headA is None or headB is None:
+            return None
         tailA = headA
         while tailA.next:
             tailA = tailA.next
         tailA.next = headA
         slow = headB
         fast = headB
+        circle = False
         while fast and fast.next:
-            pass
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                circle = True
+                break
+        if not circle:
+            tailA.next = None
+            return None
+        start = headB
+        while start and slow:
+            if start == slow:
+                tailA.next = None
+                return start
+            start = start.next
+            slow = slow.next
