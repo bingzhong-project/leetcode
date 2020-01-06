05ec34391301a6f8bb20244c1ce3bda317b57bc8
zhongyongbin <zhongyongbin@foxmail.com>
2020-01-06 19:17:08 +0800

    完成

--- algorithms/intersection-of-two-linked-lists/src/Solution.py
+++ algorithms/intersection-of-two-linked-lists/src/Solution.py
@@ -6,5 +6,34 @@ class ListNode:
 
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        pass
+    def getIntersectionNode(self, headA: ListNode,
+                            headB: ListNode) -> ListNode:
+        a_size = 0
+        b_size = 0
+
+        node = headA
+        while node:
+            a_size += 1
+            node = node.next
+
+        node = headB
+        while node:
+            b_size += 1
+            node = node.next
+
+        if a_size > b_size:
+            l_node = headA
+            s_node = headB
+        else:
+            l_node = headB
+            s_node = headA
+        diff = abs(a_size - b_size)
+        for i in range(diff):
+            l_node = l_node.next
+        while l_node and s_node:
+            if l_node == s_node:
+                return l_node
+            l_node = l_node.next
+            s_node = s_node.next
+
+        return None
