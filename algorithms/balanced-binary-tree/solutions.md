8da5df5e879b3960c54de955ad740f412790198f
zhongyongbin <zhongyongbin@foxmail.com>
2020-01-07 23:02:45 +0800

    update

--- algorithms/balanced-binary-tree/solutions.md
+++ algorithms/balanced-binary-tree/solutions.md
@@ -1,1 +1,9 @@
 # [Balanced Binary Tree](https://leetcode.com/problems/balanced-binary-tree/)
+
+## 知识点
+
+二叉树，深度优先搜索
+
+## 解题思路
+
+分别求各个节点的左右子树高度，如果存在节点，它的左右子树高度差大于 1 ，即不是平衡二叉树，否则，即是。
--- algorithms/balanced-binary-tree/src/Solution.py
+++ algorithms/balanced-binary-tree/src/Solution.py
@@ -8,4 +8,19 @@ class TreeNode:
 
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
-        pass
+        self.res = True
+
+        def dfs(node):
+            if node is None:
+                return 0
+            left_height = dfs(node.left) + 1
+            right_height = dfs(node.right) + 1
+
+            if abs(left_height - right_height) > 1:
+                self.res = False
+
+            return max(left_height, right_height)
+
+        dfs(root)
+
+        return self.res
