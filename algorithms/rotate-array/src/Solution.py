37fe9a39b3409e02c2fe519699ae5e42048137b3
zhongyongbin <zhongyongbin@foxmail.com>
2020-01-13 23:15:12 +0800

    完成

--- algorithms/rotate-array/src/Solution.py
+++ algorithms/rotate-array/src/Solution.py
@@ -3,4 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pass
+        k = (k + len(nums)) % len(nums)
+        count = 0
+        for start in range(len(nums)):
+            if count >= len(nums):
+                break
+            index = start
+            num = nums[start]
+            while True:
+                cursor = (index + len(nums) + k) % len(nums)
+                temp = nums[cursor]
+                nums[cursor] = num
+                num = temp
+                index = cursor
+                count += 1
+                if start == cursor:
+                    break
