4d0af5c3a89648671ef10b2683149a9f61ce4ae5
zhongyongbin <zhongyongbin@foxmail.com>
2020-01-15 23:29:07 +0800

    update

--- algorithms/boats-to-save-people/solutions.md
+++ algorithms/boats-to-save-people/solutions.md
@@ -1,1 +1,9 @@
 # [Boats to Save People](https://leetcode.com/problems/boats-to-save-people/)
+
+## 知识点
+
+贪心算法，双指针
+
+## 解题思路
+
+以贪心的方式，每次都优先尝试将当前最重的，和当前最轻的放一起，这样就可以达到最小的船数。
--- algorithms/boats-to-save-people/src/Solution.py
+++ algorithms/boats-to-save-people/src/Solution.py
@@ -1,3 +1,12 @@
 class Solution:
     def numRescueBoats(self, people: list, limit: int) -> int:
-        pass
+        people.sort()
+        res = 0
+        left = 0
+        right = len(people) - 1
+        while left <= right:
+            res += 1
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+        return res
