0986fe3611ee440f85800eaef9a8063922b1e3be
zhongyongbin <zhongyongbin@foxmail.com>
2020-01-18 12:09:24 +0800

    完成

--- algorithms/smallest-subtree-with-all-the-deepest-nodes/solutions.md
+++ algorithms/smallest-subtree-with-all-the-deepest-nodes/solutions.md
@@ -1,1 +1,9 @@
 # [Smallest Subtree with all the Deepest Nodes](https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/)
+
+## 知识点
+
+树
+
+## 解题思路
+
+通过深度优先搜索遍历结点，分别求出左右子树的最大深度，如果结点的左右最大深度相等，并且大于等于当前最大深度，即表明该结点的左右子树包含了最深结点，当前结点即为结果。
--- algorithms/smallest-subtree-with-all-the-deepest-nodes/src/Solution.py
+++ algorithms/smallest-subtree-with-all-the-deepest-nodes/src/Solution.py
@@ -8,4 +8,19 @@ class TreeNode:
 
 class Solution:
     def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
-        pass
+        def dfs(node, high, max_high, res):
+            if node is None:
+                return high
+
+            left_high = dfs(node.left, high + 1, max_high, res)
+            right_high = dfs(node.right, high + 1, max_high, res)
+            if left_high == right_high and left_high >= max_high[0]:
+                res[0] = node
+                max_high[0] = max(left_high, right_high)
+
+            return max(left_high, right_high)
+
+        res = [None]
+        dfs(root, 0, [0], res)
+
+        return res[0]
