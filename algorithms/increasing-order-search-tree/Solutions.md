2a4935d920d05584025e07f5ae4e57eea5b99226
zhongyongbin <zhongyongbin@foxmail.com>
2020-02-25 21:28:37 +0800

    完成

--- algorithms/increasing-order-search-tree/Solutions.md
+++ algorithms/increasing-order-search-tree/Solutions.md
@@ -1,1 +1,9 @@
-# [Increasing Order Search Tree](https://leetcode-cn.com/problems/increasing-order-search-tree/)
+# [Increasing Order Search Tree](https://leetcode.com/problems/increasing-order-search-tree/)
+
+## 知识点
+
+树
+
+## 解题思路
+
+对树进行中序遍历，逐个的将树重组
--- algorithms/increasing-order-search-tree/src/Solution.py
+++ algorithms/increasing-order-search-tree/src/Solution.py
@@ -8,4 +8,19 @@ class TreeNode:
 
 class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        pass
+        self.p = TreeNode(None)
+
+        def func(node):
+            if node.left:
+                func(node.left)
+            node.left = None
+            self.p.right = node
+            self.p = self.p.right
+            if node.right:
+                func(node.right)
+
+        sentinel = TreeNode(None)
+        sentinel.right = self.p
+        func(root)
+
+        return sentinel.right
