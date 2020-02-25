8ab23bbca35a4df76b83965b14d52a3e9612358f
zhongyongbin <zhongyongbin@foxmail.com>
2020-02-25 21:52:09 +0800

    完成

--- algorithms/range-sum-of-bst/src/Solution.py
+++ algorithms/range-sum-of-bst/src/Solution.py
@@ -8,4 +8,18 @@ class TreeNode:
 
 class Solution:
     def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
-        pass
+        self.res = 0
+
+        def func(node, l, r):
+            if node.val >= l and node.val <= r:
+                self.res += node.val
+                func(node.left, l, r)
+                func(node.right, l, r)
+            elif node.val < l:
+                func(node.right, l, r)
+            elif node.val > r:
+                func(node.left, l, r)
+
+        func(root, L, R)
+
+        return self.res
