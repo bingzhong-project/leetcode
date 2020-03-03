68ef0cda58595490b6547f47e44fffbf30e9a668
zhongyongbin <zhongyongbin@foxmail.com>
2020-03-03 10:36:45 +0800

    完成

--- algorithms/maximum-binary-tree-ii/solutions.md
+++ algorithms/maximum-binary-tree-ii/solutions.md
@@ -1,1 +1,9 @@
 # [Maximum Binary Tree II](https://leetcode.com/problems/maximum-binary-tree-ii/)
+
+## 知识点
+
+二叉树
+
+## 解题思路
+
+先将树转化为数组，在数组中插入给定值，然后根据新的数组构建树。
--- algorithms/maximum-binary-tree-ii/src/Solution.py
+++ algorithms/maximum-binary-tree-ii/src/Solution.py
@@ -8,4 +8,25 @@ class TreeNode:
 
 class Solution:
     def insertIntoMaxTree(self, root: TreeNode, val: int) -> TreeNode:
-        pass
+        def tree2array(root):
+            if root is None:
+                return []
+            left_array = tree2array(root.left)
+            right_array = tree2array(root.right)
+
+            return left_array + [root.val] + right_array
+
+        def array2tree(array):
+            if len(array) == 0:
+                return None
+            max_val = max(array)
+            index = array.index(max_val)
+            node = TreeNode(max_val)
+            node.left = array2tree(array[:index])
+            node.right = array2tree(array[index + 1:])
+            return node
+
+        array = tree2array(root)
+        array.append(val)
+
+        return array2tree(array)
