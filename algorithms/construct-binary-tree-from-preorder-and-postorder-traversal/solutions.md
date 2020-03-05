487f4aa56454fedb3fe497b2c0d23626b2101223
zhongyongbin <zhongyongbin@foxmail.com>
2020-03-05 10:32:35 +0800

    完成

--- algorithms/construct-binary-tree-from-preorder-and-postorder-traversal/solutions.md
+++ algorithms/construct-binary-tree-from-preorder-and-postorder-traversal/solutions.md
@@ -1,1 +1,14 @@
 # [Construct Binary Tree from Preorder and Postorder Traversal](https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/)
+
+## 知识点
+
+树，树的遍历
+
+## 解题思路
+
+分析树的先序遍历与后序遍历，可以得出以下结论：
+
+1. 在先序遍历 pre 中，以 pre[i] 作为根节点，pre[i + 1] 是其左子节点。
+2. 在后序遍历 post 中，以 post[i] 作为根节点，post[i - 1] 是其右子节点。
+
+根据上述的结论，在处理时，总是以 pre[0] 作为根节点，并通过 post 找出其右子节点，以此构建出完整的二叉树。
--- algorithms/construct-binary-tree-from-preorder-and-postorder-traversal/src/Solution.py
+++ algorithms/construct-binary-tree-from-preorder-and-postorder-traversal/src/Solution.py
@@ -7,5 +7,21 @@ class TreeNode:
 
 
 class Solution:
-    def constructFromPrePost(self, pre: List[int], post: List[int]) -> TreeNode:
-        pass
+    def constructFromPrePost(self, pre: list, post: list) -> TreeNode:
+        def construct(pre, post):
+            if len(pre) == 0:
+                return None
+            if len(pre) == 1:
+                return TreeNode(pre[0])
+            root_val = pre[0]
+            root_post_index = post.index(root_val)
+            right_val = post[root_post_index - 1]
+            right_pre_index = pre.index(right_val)
+
+            root = TreeNode(root_val)
+            root.left = construct(pre[1:right_pre_index], post)
+            root.right = construct(pre[right_pre_index:], post)
+
+            return root
+
+        return construct(pre, post)
