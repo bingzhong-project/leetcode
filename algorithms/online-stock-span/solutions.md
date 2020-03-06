6b1e3689a1c9061a62e1dfb68633d6f2c0994acc
zhongyongbin <zhongyongbin@foxmail.com>
2020-03-06 18:57:49 +0800

    完成

--- algorithms/online-stock-span/solutions.md
+++ algorithms/online-stock-span/solutions.md
@@ -1,1 +1,9 @@
 # [Online Stock Span](https://leetcode.com/problems/online-stock-span/)
+
+## 知识点
+
+栈
+
+## 解题思路
+
+利用自底向上单调递减的栈存储每一天的价钱和跨度，当不满足单调栈定义时，栈顶元素出栈，新入栈元素的跨度为加上所有出栈元素的跨度。
--- algorithms/online-stock-span/src/Solution.py
+++ algorithms/online-stock-span/src/Solution.py
@@ -1,6 +1,11 @@
 class StockSpanner:
     def __init__(self):
-        pass
+        self.stack = []
 
     def next(self, price: int) -> int:
-        pass
+        count = 1
+        while self.stack and self.stack[-1][0] <= price:
+            last_price, last_count = self.stack.pop()
+            count += last_count
+        self.stack.append((price, count))
+        return count
