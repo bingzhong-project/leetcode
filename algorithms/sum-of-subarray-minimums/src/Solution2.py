ba82e7d94840b3fec272e4c5f82e3a2cfe4b0505
zhongyongbin <zhongyongbin@foxmail.com>
2020-03-10 11:43:07 +0800

    完成

--- algorithms/sum-of-subarray-minimums/src/Solution2.py
+++ algorithms/sum-of-subarray-minimums/src/Solution2.py
@@ -1,22 +1,23 @@
 class Solution:
     def sumSubarrayMins(self, A: list) -> int:
-        """TLE
-        """
         MOD = 10**9 + 7
         dp = [0 for _ in range(len(A))]
         dp[0] = A[0]
         res = dp[0]
+        stack = []
+        stack.append(0)
         for i in range(1, len(A)):
+            while stack and A[stack[-1]] > A[i]:
+                stack.pop()
             if A[i] >= A[i - 1]:
                 dp[i] = A[i] + dp[i - 1]
             else:
-                j = i - 1
-                while j >= 0 and A[i] <= A[j]:
-                    j -= 1
+                j = stack[-1] if stack else -1
                 if j < 0:
                     dp[i] = A[i] * (i + 1)
                 else:
                     dp[i] = A[i] * (i - j) + dp[j]
             res = (res + dp[i]) % MOD
+            stack.append(i)
 
         return res
